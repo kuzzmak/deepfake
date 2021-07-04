188753ece8a23ef11e24b044c4a5e3d7deeac549
kuzzmak <ak50380@fer.hr>
2021-07-04 23:30:27 +0200

    Different names used throghout the app.

---
+++ names.py
@@ -0,0 +1,7 @@
+APP_NAME = 'Deepfake tool'
+
+START_PAGE_NAME = 'start_page'
+START_PAGE_TITLE = 'Start page'
+
+MAKE_DEEPFAKE_PAGE_NAME = 'make_deepfake_page'
+MAKE_DEEPFAKE_PAGE_TITLE = 'Make deepfake page'
