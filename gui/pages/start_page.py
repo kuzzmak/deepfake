625467e7b07740acc2e4a394fcc4815c0bb26b9d
kuzzmak <ak50380@fer.hr>
2021-07-04 23:31:39 +0200

    Start page.

---
+++ gui/pages/start_page.py
@@ -0,0 +1,20 @@
+from gui.pages.page import Page
+from gui.templates.start_page import Ui_start_page
+
+from names import START_PAGE_NAME, MAKE_DEEPFAKE_PAGE_NAME, START_PAGE_TITLE
+
+
+class StartPage(Page, Ui_start_page):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setupUi(self)
+        self.name = START_PAGE_NAME
+
+        self.setWindowTitle(START_PAGE_TITLE)
+
+        self.make_deepfake_btn.clicked.connect(self.goto_make_deepfake)
+
+    def goto_make_deepfake(self):
+        self.goto(MAKE_DEEPFAKE_PAGE_NAME)
