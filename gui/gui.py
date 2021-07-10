0ed4c9f74c8faaf888163e87bd1e7616ea165239
kuzzmak <ak50380@fer.hr>
2021-07-10 12:17:02 +0200

    Changed imports.

--- gui/gui.py
+++ gui/gui.py
@@ -1,19 +1,20 @@
-from names import APP_NAME
-from gui.pages.make_deepfake_page import MakeDeepfakePage
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QMainWindow, QStackedWidget
+import PyQt5.QtCore as qtc
+import PyQt5.QtWidgets as qwt
 
 from gui.pages.start_page import StartPage
+from gui.pages.make_deepfake_page import MakeDeepfakePage
 from gui.pages.page import Page
-from constants import MAX_HEIGHT, MAX_WIDTH
+
+from names import APP_NAME
+from constants import MAX_HEIGHT, MAX_WIDTH, PREFERRED_HEIGHT, PREFERRED_WIDTH
 
 
-class App(QMainWindow):
+class App(qwt.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.stacked_widget = QStackedWidget()
+        self.stacked_widget = qwt.QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
         self.m_pages = {}
@@ -22,7 +23,7 @@ class App(QMainWindow):
         # self.setGeometry(0, 0, MAX_WIDTH, MAX_HEIGHT)
         # self.setMaximumHeight(MAX_HEIGHT)
         # self.setMaximumWidth(MAX_WIDTH)
-
+        self.resize(PREFERRED_WIDTH, PREFERRED_HEIGHT)
         self.setWindowTitle(APP_NAME)
 
     def register_page(self, page: Page):
@@ -35,7 +36,7 @@ class App(QMainWindow):
         self.register_page(StartPage())
         self.register_page(MakeDeepfakePage())
 
-    @QtCore.pyqtSlot(str)
+    @qtc.pyqtSlot(str)
     def goto(self, name):
         if name in self.m_pages:
             page = self.m_pages[name]
