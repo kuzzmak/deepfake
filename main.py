9a94bf456957903184fe766b0513db7fe49a81ac
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-10 18:17:46 +0200

    Refactored.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-
-import PyQt5.QtWidgets as qwt
-from gui.gui import App
+from app import App
 
 
 if __name__ == '__main__':
-    _app = qwt.QApplication(sys.argv)
     app = App()
-    app.show()
-    sys.exit(_app.exec_())
+    app.gui()
