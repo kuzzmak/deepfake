433e2d6deb71eb6af48784a08e11be188b572e3b
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-17 23:44:04 +0200

    Worker which should do io operations and maybe notify gui about success of those operations.

---
+++ gui/workers/io_worker.py
@@ -0,0 +1,29 @@
+import os
+
+import PyQt5.QtCore as qtc
+
+from enums import IO_OP_TYPE
+from common_structures import IO_OP
+
+
+class IO_Worker(qtc.QObject):
+
+    # incremented_val = qtc.pyqtSignal(int)
+    io_op_successful_sig = qtc.pyqtSignal(bool)
+
+    @qtc.pyqtSlot(IO_OP)
+    def io_op(self, op: IO_OP):
+        if op.type == IO_OP_TYPE.DELETE:
+            self.delete(op.value)
+
+    def delete(self, value: str):
+        exists = os.path.exists(value)
+        print('from worker: ', value, exists)
+
+    # @qtc.pyqtSlot(int)
+    # def increment_value(self, value: int):
+    #     new_value = value
+    #     while new_value < 100:
+    #         new_value += 1
+    #         time.sleep(0.05)
+    #         self.incremented_val.emit(new_value)
