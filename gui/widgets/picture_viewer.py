a4d4939180d9b8359c2f83bc32094da0279f43f9
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-17 19:27:37 +0200

    It's now possible to send image path to viewe or numpy array which represents image.

--- gui/widgets/picture_viewer.py
+++ gui/widgets/picture_viewer.py
@@ -1,27 +1,46 @@
 import os
-import sys
-from typing import List
+from typing import List, Union
+
+import numpy as np
+
 import PyQt5.QtGui as qtg
 import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qwt
 
+from utils import np_array_to_qicon, qicon_from_path
+
+DEFAULT_ROLE = qtc.Qt.UserRole + 1
+
 
 class StandardItem(qtg.QStandardItem):
-    PathRole = qtc.Qt.UserRole + 1
+
+    DataRole = DEFAULT_ROLE
+    NameRole = qtc.Qt.UserRole + 2
+    PathRole = qtc.Qt.UserRole + 3
 
     def __init__(self, *args, **kwargs):
         super(StandardItem, self).__init__(*args, **kwargs)
+        self.name = ''
         self.path = ''
+        self.image = ''
 
-    def setData(self, value, role=qtc.Qt.UserRole + 1):
+    def setData(self, value, role=DEFAULT_ROLE):
         if role == StandardItem.PathRole:
             self.path = value
+        elif role == StandardItem.NameRole:
+            self.name = value
+        elif role == StandardItem.DataRole:
+            self.image = value
         else:
             qtg.QStandardItem.setData(self, value, role)
 
-    def data(self, role=qtc.Qt.UserRole + 1):
+    def data(self, role=DEFAULT_ROLE):
         if role == StandardItem.PathRole:
             return self.path
+        elif role == StandardItem.NameRole:
+            return self.name
+        elif role == StandardItem.DataRole:
+            return self.image
         return qtg.QStandardItem.data(self, role)
 
     def type(self):
@@ -29,19 +48,33 @@ class StandardItem(qtg.QStandardItem):
 
 
 class StandardItemModel(qtg.QStandardItemModel):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setItemPrototype(StandardItem())
 
     def data(self, index, role=qtc.Qt.DisplayRole):
         if role == qtc.Qt.DecorationRole:
+
             it = self.itemFromIndex(index)
-            value = it.data(qtc.Qt.DecorationRole)
-            if value is None:
-                path = it.data(StandardItem.PathRole)
-                value = qtg.QIcon(qtg.QPixmap(path))
-                it.setData(value, qtc.Qt.DecorationRole)
-            return value
+            item = it.data(qtc.Qt.DecorationRole)
+
+            if item is None:
+
+                item_data = it.data(StandardItem.DataRole)
+                path = it.data(StandardItem.DataRole)
+
+                if isinstance(item_data, str):
+                    image = qicon_from_path(path)
+                elif isinstance(item_data, np.ndarray):
+                    image = np_array_to_qicon(item_data)
+
+                it.setData(image, qtc.Qt.DecorationRole)
+                name = it.data(StandardItem.NameRole)
+                it.setText(name)
+
+            return item
+
         elif role == qtc.Qt.SizeHintRole:
             return qtc.QSize(300, 200)
         else:
@@ -69,12 +102,12 @@ class PictureViewer(qwt.QWidget):
 
         self.pictures_added_sig.connect(self.pictures_added)
 
+        self.init_ui()
+
+    def init_ui(self):
         self.ui_image_viewer = qwt.QListView()
         self.ui_image_viewer.viewport().installEventFilter(self)
         self.ui_image_viewer.setSpacing(10)
-        # self.ui_image_viewer.installEventFilter(self)
-        # self.ui_image_viewer.mousePressEvent.connect(self.item_clicked)
-        # self.ui_image_viewer.clicked.connect(self.item_clicked)
         self.ui_image_viewer.setViewMode(qwt.QListView.IconMode)
         self.ui_image_viewer.setResizeMode(qwt.QListView.Adjust)
         self.ui_image_viewer.setEditTriggers(
@@ -84,30 +117,64 @@ class PictureViewer(qwt.QWidget):
         self.ui_image_viewer.setModel(StandardItemModel())
 
         grid = qwt.QVBoxLayout()
-        # grid.setContentsMargins(10, 10, 10, 10)
         grid.addWidget(self.ui_image_viewer)
-
         self.setLayout(grid)
+
         sizePolicy = qwt.QSizePolicy(
             qwt.QSizePolicy.Preferred, qwt.QSizePolicy.MinimumExpanding)
         self.setSizePolicy(sizePolicy)
 
+        self.init_context_menu()
+
+    def init_context_menu(self):
         self.context_menu = qwt.QMenu()
         self.cmef = ContextMenuEventFilter(self.context_menu)
         self.context_menu.installEventFilter(self.cmef)
-        newAct = self.context_menu.addAction("New")
-        openAct = self.context_menu.addAction("Open")
-        quitAct = self.context_menu.addAction("Quit")
+        rename = self.context_menu.addAction("Rename")
+        delete = self.context_menu.addAction("Delete")
+        rename.triggered.connect(self.rename_selected_picture)
+        delete.triggered.connect(self.remove_selected_picture)
+
+    def remove_selected_picture(self):
+        index = self.ui_image_viewer.selectionModel().currentIndex()
+        print(self.ui_image_viewer.model().data(index, qtc.Qt.UserRole + 1))
+
+        self.ui_image_viewer.model().removeRow(index.row())
+
+    def rename_selected_picture(self):
+
+        index = self.ui_image_viewer.selectionModel().currentIndex()
+        print(self.ui_image_viewer.model().data(index, qtc.Qt.UserRole + 1))
+
+        self.ui_image_viewer.model().removeRow(index.row())
+
+        # print(index)
+        # print(self.ui_image_viewer.model().data(index[0], qtc.Qt.UserRole+1))
 
     @qtc.pyqtSlot(list)
-    def pictures_added(self, img_paths: List[str]):
-        for img_path in img_paths:
-            name = os.path.splitext(os.path.basename(img_path))[0]
-            item = StandardItem(name)
-            item.setData(img_path)
+    def pictures_added(self, images: List[Union[str, np.ndarray]]):
+        """pyqtSlot which triggers when new image paths or images
+        in form of an `np.ndarray` are emitted to show in `ImageViewer`.
+
+        Parameters
+        ----------
+        images : List[Union[str, np.ndarray]]
+            list of image paths or `np.ndarray`s
+        """
+        for image in images:
+            item = StandardItem()
+            if isinstance(image, str):
+                name = os.path.splitext(os.path.basename(image))[0]
+                item.setData(image, StandardItem.PathRole)
+                item.setData(image, StandardItem.DataRole)
+            else:
+                name = str(self.ui_image_viewer.model().rowCount())
+                item.setData(image, StandardItem.DataRole)
+                item.setData(name, StandardItem.PathRole)
+            item.setData(name, StandardItem.NameRole)
             self.ui_image_viewer.model().appendRow(item)
 
-    def eventFilter(self, source, event) -> bool:
+    def eventFilter(self, source: qtc.QObject, event: qtc.QEvent) -> bool:
         if source == self.ui_image_viewer.viewport():
             if event.type() == qtc.QEvent.MouseButtonPress:
                 self.context_menu.close()
