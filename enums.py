8156d4a507b184ca939003a09087079ca4555fdb
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-18 16:59:27 +0200

    New dialog enums.

--- enums.py
+++ enums.py
@@ -1,5 +1,18 @@
 from enum import Enum
 
+
 class IO_OP_TYPE(Enum):
     DELETE = 'delete'
     RENAME = 'rename'
+
+
+class DIALOG_MESSAGE_ICON(Enum):
+    DELETE = ':/delete.svg'
+    RENAME = ':/rename.svg'
+    WARNING = ':/warning.svg'
+
+
+class DIALOG_MESSAGE_TYPE(Enum):
+    DELETE = 'Delete'
+    RENAME = 'Rename'
+    WARNING = 'Warning'
