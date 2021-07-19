fe0aba96c6127678c9ffb5b8cb55194aecac7d71
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-19 23:33:59 +0200

    Base class for worker threads.

---
+++ gui/workers/threads/worker_thread.py
@@ -0,0 +1,23 @@
+import PyQt5.QtCore as qtc
+
+from gui.workers.worker import Worker
+
+
+class WorkerThread(qtc.QThread):
+
+    def __init__(self, worker: Worker, *args, **kwargs):
+        """Base class for any worker related threads.
+
+        Parameters
+        ----------
+        worker : Worker
+            Worker class which should be in it's separate thread
+            so gui doesn't freeze
+        """
+        super().__init__(*args, **kwargs)
+
+        self.worker = worker
+        self.worker.moveToThread(self)
+
+    def run(self) -> None:
+        return super().run()
