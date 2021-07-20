9ffb3f0444b4a01fc0ec6300802981c5c8b077c8
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-20 23:50:09 +0200

    Testing new system with messages.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sys
+import json
 
 import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qwt
 
 from gui.pages.main_page import MainPage
-from gui.workers.io_worker import IO_Worker
+from gui.workers.threads.io_worker_thread import IO_WorkerThread
+from gui.workers.threads.message_worker_thread import MessageWorkerThread
 
 from common_structures import IO_OP
 
@@ -16,14 +18,29 @@ class App(qtc.QObject):
     def __init__(self):
         super().__init__()
 
+        self.load_config()
+
         self.setup_io_worker()
+        self.setup_message_worker()
 
     def setup_io_worker(self):
-        self.io_worker = IO_Worker()
-        self.io_worker_thread = qtc.QThread()
-        self.io_op_sig.connect(self.io_worker.io_op)
-        self.io_worker.moveToThread(self.io_worker_thread)
+        self.io_worker_thread = IO_WorkerThread()
         self.io_worker_thread.start()
+
+    def setup_message_worker(self):
+        self.message_worker_thread = MessageWorkerThread()
+        self.message_worker_thread.start()
+
+    def load_config(self):
+        with open('app_config.json') as f:
+            self.conf = json.load(f)
+
+    # def setup_io_worker(self):
+    #     self.io_worker = IO_Worker()
+    #     self.io_worker_thread = qtc.QThread()
+    #     self.io_op_sig.connect(self.io_worker.io_op)
+    #     self.io_worker.moveToThread(self.io_worker_thread)
+    #     self.io_worker_thread.start()
 
     def gui(self):
         _app = qwt.QApplication(sys.argv)
