ea7f8fa8fe000065172fdfab7de59caadb3e0ecf
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-22 00:24:48 +0200

    Moved QThreads in QApplication.

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qwt
 
 from gui.pages.main_page import MainPage
-from gui.workers.threads.io_worker_thread import IO_WorkerThread
-from gui.workers.threads.message_worker_thread import MessageWorkerThread
 
 from common_structures import IO_OP
 
@@ -20,23 +18,12 @@ class App(qtc.QObject):
 
         self.load_config()
 
-        self.setup_io_worker()
-        self.setup_message_worker()
-
-    def setup_io_worker(self):
-        self.io_worker_thread = IO_WorkerThread()
-        self.io_worker_thread.start()
-
-    def setup_message_worker(self):
-        self.message_worker_thread = MessageWorkerThread()
-        self.message_worker_thread.start()
-
     def load_config(self):
         with open('app_config.json') as f:
             self.conf = json.load(f)
 
     def gui(self):
         _app = qwt.QApplication(sys.argv)
-        gui = MainPage(self)
+        gui = MainPage()
         gui.show()
         sys.exit(_app.exec_())
--- enums.py
+++ enums.py
@@ -1,4 +1,20 @@
+from collections import namedtuple
 from enum import Enum
+
+ConsolePrefix = namedtuple('ConsolePrefix', 'prefix prefix_color')
+
+
+class CONSOLE_COLORS(Enum):
+    RED = '#ff0000'
+    BLACK = '#000000'
+    ORANGE = '#ffa500'
+
+
+class CONSOLE_MESSAGE_TYPE(Enum):
+    LOG = ConsolePrefix('[LOG]', CONSOLE_COLORS.BLACK)
+    INFO = ConsolePrefix('[INFO]', CONSOLE_COLORS.BLACK)
+    ERROR = ConsolePrefix('[ERROR]', CONSOLE_COLORS.RED)
+    WARNING = ConsolePrefix('[WARNING]', CONSOLE_COLORS.ORANGE)
 
 
 class IO_OP_TYPE(Enum):
