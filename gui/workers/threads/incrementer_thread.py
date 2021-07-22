d21b5342de39b7bf3e0afe760b852e70441d0df0
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-22 23:48:18 +0200

    Removed.

--- gui/workers/threads/incrementer_thread.py
+++
@@ -1,8 +0,0 @@
-from gui.workers.threads.worker_thread import WorkerThread
-from gui.workers.worker import IncrementerWorker
-
-
-class IncrementerThread(WorkerThread):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(IncrementerWorker(), *args, **kwargs)
