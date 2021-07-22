562ee636fc0367e601999c64fc8e563f5dd0f497
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-22 23:47:49 +0200

    Removed unnecessary code, added function for adding signals to worker.

--- gui/workers/worker.py
+++ gui/workers/worker.py
@@ -1,73 +1,29 @@
 # import time
-from message.message import Message
 import abc
 import PyQt5
-
 import PyQt5.QtCore as qtc
 
-# class Worker(qtc.QObject):
+from message.message import Message
 
-#     incremented_val = qtc.pyqtSignal(int)
-
-#     @qtc.pyqtSlot(int)
-#     def increment_value(self, value: int):
-#         new_value = value
-#         while new_value < 100:
-#             new_value += 1
-#             time.sleep(0.05)
-#             self.incremented_val.emit(new_value)
+from enums import SIGNAL_OWNER
 
 
 class WorkerMeta(PyQt5.sip.wrappertype, abc.ABCMeta):
     ...
 
 
-class Worker(qtc.QObject, metaclass=WorkerMeta):
-
-    succesful_op = qtc.pyqtSignal(bool)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.succesful_op.connect(self.print)
-
-    @qtc.pyqtSlot(bool)
-    def print(self, val: bool):
-        print('val: ', val)
-
-    @abc.abstractclassmethod
-    def process(self, data):
-        ...
-
-
 class NewWorker(qtc.QObject, metaclass=WorkerMeta):
 
     succesful_op = qtc.pyqtSignal(bool)
+    signals = dict()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.succesful_op.connect(self.print)
 
-    @qtc.pyqtSlot(bool)
-    def print(self, val: bool):
-        print('val: ', val)
+    def add_signal(self, sig: qtc.pyqtSignal, sig_owner: SIGNAL_OWNER):
+        self.signals[sig_owner] = sig
 
     @abc.abstractclassmethod
     @qtc.pyqtSlot(Message)
     def process(self, msg: Message):
         ...
-
-
-class IncrementerWorker(Worker):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def process(self, data):
-        print('data from inc worker: ', data)
-        self.succesful_op.emit(False)
-
-
-if __name__ == '__main__':
-
-    wor = IncrementerWorker()
-    wor.process('s')
