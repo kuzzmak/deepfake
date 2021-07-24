8fae8a0daf3305f14cf84a43b04416c970f6f17c
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-25 01:39:42 +0200

    Added functionality for frame extraction.

--- gui/workers/frames_extraction_worker.py
+++ gui/workers/frames_extraction_worker.py
@@ -1,4 +1,9 @@
-from message.message import Message
+from enums import IO_OP_TYPE, MESSAGE_TYPE, SIGNAL_OWNER
+import os
+
+import cv2 as cv
+
+from message.message import IO_OperationMessageBody, Message
 
 from gui.workers.worker import Worker
 
@@ -8,5 +13,29 @@ class FramesExtractionWorker(Worker):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def process(msg: Message):
-        print('heeelo')
+    def process(self, msg: Message):
+        video_path, dest_dir, image_format = msg.body.get_data()
+
+        vidcap = cv.VideoCapture(video_path)
+        success, image = vidcap.read()
+
+        total_frames = int(vidcap.get(cv.CAP_PROP_FRAME_COUNT))
+
+        count = 0
+        while success:
+            im_path = os.path.join(dest_dir, f'frame_{count}.jpg')
+
+            msg = Message(
+                MESSAGE_TYPE.REQUEST,
+                IO_OperationMessageBody(
+                    IO_OP_TYPE.SAVE,
+                    im_path,
+                    None,
+                    image,
+                )
+            )
+
+            self.signals[SIGNAL_OWNER.IO_WORKER].emit(msg)
+
+            success, image = vidcap.read()
+            count += 1
