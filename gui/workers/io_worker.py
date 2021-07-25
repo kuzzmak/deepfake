5251db24494c65fe4522386385c74efeeaa85bf5
kuzzmak <kuzminski.antonio@gmail.com>
2021-07-25 19:17:58 +0200

    Multipart messages update progressbar.

--- gui/workers/io_worker.py
+++ gui/workers/io_worker.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 
-from message.message import Message
+from message.message import AnswerBody, Message
 
 from gui.workers.worker import Worker
 
-from enums import IO_OP_TYPE
+from enums import IO_OP_TYPE, MESSAGE_STATUS, MESSAGE_TYPE, SIGNAL_OWNER
 
 
 class IO_Worker(Worker):
@@ -14,8 +14,19 @@ class IO_Worker(Worker):
 
     def process(self, msg: Message):
 
-        op_type, file_path, new_file_path, file = msg.body.get_data()
+        op_type, file_path, new_file_path, file, multipart, part, total = msg.body.get_data()
 
         if op_type == IO_OP_TYPE.SAVE:
             if file is not None:
                 cv.imwrite(file_path, file)
+                if multipart:
+                    msg = Message(
+                        MESSAGE_TYPE.ANSWER,
+                        AnswerBody(
+                            MESSAGE_STATUS.OK,
+                            part == total,
+                        )
+                    )
+                    self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(msg)
+                else:
+                    ...
