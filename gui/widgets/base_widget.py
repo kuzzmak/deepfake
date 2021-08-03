048e907957c60cbd9e80fb974c9803f29caf1d03
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-03 17:00:28 +0200

    Method for enabling or disabling widget.

--- gui/widgets/base_widget.py
+++ gui/widgets/base_widget.py
@@ -10,12 +10,25 @@ class BaseWidget(qwt.QWidget):
 
     _signals: Dict[SIGNAL_OWNER, qtc.pyqtSignal]
 
-    def __init__(self, signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict()):
+    def __init__(self,
+                 signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict()):
         """Base widget class. Contains dictionary of signals
         where one could add signal for any kind of work.
         """
         super().__init__()
         self._signals = signals
+
+    def enable_widget(self, widget: qwt.QWidget, enabled: bool = True):
+        """Enables or disables some widget.
+
+        Parameters
+        ----------
+        widget : qwt.QWidget
+            widget to enable or disable
+        enabled : bool, optional
+            enable or disable widget, by default True
+        """
+        widget.setEnabled(enabled)
 
     @property
     def signals(self) -> Dict[SIGNAL_OWNER, qtc.pyqtSignal]:
