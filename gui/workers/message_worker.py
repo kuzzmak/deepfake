1a8f396549323dd8a11d0c97e52b0ae324277031
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-04 19:50:53 +0200

    Simplified message worker.

--- gui/workers/message_worker.py
+++ gui/workers/message_worker.py
@@ -1,7 +1,6 @@
-from enums import SIGNAL_OWNER
 from gui.workers.worker import Worker
 
-from message.message import JOB_TYPE, MESSAGE_TYPE, Message
+from message.message import Message
 
 
 class MessageWorker(Worker):
@@ -10,32 +9,4 @@ class MessageWorker(Worker):
         super().__init__(*args, **kwargs)
 
     def process(self, msg: Message):
-
-        # job_type = msg.body.job_type
-
-        if msg.type == MESSAGE_TYPE.REQUEST:
-
-            self.signals[msg.recipient].emit(msg)
-
-            # if msg.recipient == SIGNAL_OWNER.FRAMES_EXTRACTION_WORKER:
-            #     print('je ex work')
-            #     print(msg)
-
-            # if job_type == JOB_TYPE.CONSOLE_PRINT:
-            #     self.signals[SIGNAL_OWNER.CONSOLE].emit(msg)
-
-            # elif job_type == JOB_TYPE.FRAME_EXTRACTION:
-            #     self.signals[SIGNAL_OWNER.FRAMES_EXTRACTION_WORKER].emit(msg)
-
-            # elif job_type == JOB_TYPE.IO_OPERATION:
-            #     self.signals[SIGNAL_OWNER.IO_WORKER].emit(msg)
-
-            # elif job_type == JOB_TYPE.WIDGET_CONFIGURATION:
-            #     self.signals[SIGNAL_OWNER.CONFIGURE_WIDGET].emit(msg)
-
-            # elif job_type == JOB_TYPE.FACE_DETECTION:
-            #     self.signals[SIGNAL_OWNER.FACE_DETECTION_WORKER].emit(msg)
-
-        elif msg.type == MESSAGE_TYPE.ANSWER:
-
-            self.signals[msg.recipient].emit(msg)
+        self.signals[msg.recipient].emit(msg)
