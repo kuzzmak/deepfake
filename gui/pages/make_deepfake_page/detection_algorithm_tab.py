975ef51ff01d482a9e19beb1e1f7d14e3ffcf028
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-05 18:21:23 +0200

    New functionality added.

--- gui/pages/make_deepfake_page/detection_algorithm_tab.py
+++ gui/pages/make_deepfake_page/detection_algorithm_tab.py
@@ -1,22 +1,47 @@
+from typing import Dict, Optional
+
 import PyQt5.QtGui as qtg
 import PyQt5.QtCore as qtc
 import PyQt5.QtWidgets as qwt
 
-from enums import FACE_DETECTION_ALGORITHM
+from enums import (
+    BODY_KEY,
+    CONSOLE_MESSAGE_TYPE,
+    DATA_TYPE,
+    FACE_DETECTION_ALGORITHM,
+    JOB_TYPE,
+    MESSAGE_STATUS,
+    MESSAGE_TYPE,
+    SIGNAL_OWNER,
+)
 
+from gui.widgets.base_widget import BaseWidget
 from gui.widgets.picture_viewer import PictureViewer
+
+from message.message import Body, Message, Messages
 
 from resources.icons import icons
 
 
-class DetectionAlgorithmTab(qwt.QWidget):
+class DetectionAlgorithmTab(BaseWidget):
 
-    algorithm_selected_sig = qtc.pyqtSignal(FACE_DETECTION_ALGORITHM)
+    input_picture_added_sig = qtc.pyqtSignal(Message)
+    output_picture_added_sig = qtc.pyqtSignal(Message)
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self,
+                 signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict()):
+        super().__init__(signals)
+
+        self.input_faces_directory = None
+        self.output_faces_directory = None
+        self.model_path = None
+        self.algorithm_selected_value = FACE_DETECTION_ALGORITHM.S3FD
+
+        self.input_picture_added_sig.connect(self.input_picture_added)
+        self.output_picture_added_sig.connect(self.output_picture_added)
 
         self.init_ui()
+        self.add_picture_viewer_signals()
 
     def init_ui(self):
         layout = qwt.QHBoxLayout()
@@ -42,7 +67,7 @@ class DetectionAlgorithmTab(qwt.QWidget):
         algorithm_selection_wgt_layout.addWidget(algorithm_gb)
 
         bg = qwt.QButtonGroup(algorithm_gb)
-        bg.idPressed.connect(self.face_detection_algorithm_selected)
+        bg.idPressed.connect(self.algorithm_selected)
 
         mtcnn_btn = qwt.QRadioButton(text='MTCNN', parent=algorithm_gb)
         box_group_layout.addWidget(mtcnn_btn)
@@ -68,6 +93,8 @@ class DetectionAlgorithmTab(qwt.QWidget):
             40, 20, qwt.QSizePolicy.Fixed, qwt.QSizePolicy.Fixed)
         input_directory_wgt_layout.addItem(spacer)
         select_input_faces_directory_btn = qwt.QPushButton(text='Select')
+        select_input_faces_directory_btn.clicked.connect(lambda:
+                                                         self.select_faces_directory(DATA_TYPE.INPUT))
         select_input_faces_directory_btn.setFixedWidth(120)
         input_directory_wgt_layout.addWidget(select_input_faces_directory_btn)
         left_part_layout.addWidget(input_directory_wgt)
@@ -81,10 +108,25 @@ class DetectionAlgorithmTab(qwt.QWidget):
             40, 20, qwt.QSizePolicy.Fixed, qwt.QSizePolicy.Fixed)
         output_directory_wgt_layout.addItem(spacer)
         select_output_faces_directory_btn = qwt.QPushButton(text='Select')
+        select_output_faces_directory_btn.clicked.connect(lambda:
+                                                          self.select_faces_directory(DATA_TYPE.OUTPUT))
         select_output_faces_directory_btn.setFixedWidth(120)
         output_directory_wgt_layout.addWidget(
             select_output_faces_directory_btn)
         left_part_layout.addWidget(output_directory_wgt)
+
+        model_path_wgt = qwt.QWidget()
+        model_path_wgt_layout = qwt.QHBoxLayout()
+        model_path_wgt.setLayout(model_path_wgt_layout)
+        model_path_wgt_layout.addWidget(qwt.QLabel(text='Model path'))
+        spacer = qwt.QSpacerItem(
+            40, 20, qwt.QSizePolicy.Fixed, qwt.QSizePolicy.Fixed)
+        model_path_wgt_layout.addItem(spacer)
+        select_model_path_btn = qwt.QPushButton(text='Select')
+        select_model_path_btn.clicked.connect(self.select_model_path)
+        select_model_path_btn.setFixedWidth(120)
+        model_path_wgt_layout.addWidget(select_model_path_btn)
+        left_part_layout.addWidget(model_path_wgt)
 
         spacer = qwt.QSpacerItem(
             40, 20, qwt.QSizePolicy.Expanding,
@@ -93,6 +135,7 @@ class DetectionAlgorithmTab(qwt.QWidget):
         left_part_layout.addItem(spacer)
 
         start_detection_btn = qwt.QPushButton(text='Start detection')
+        start_detection_btn.clicked.connect(self.start_detection)
         start_detection_btn.setFixedWidth(150)
         start_detection_btn.setIcon(qtg.QIcon(qtg.QPixmap(':/play.svg')))
         left_part_layout.addWidget(start_detection_btn, 0, qtc.Qt.AlignHCenter)
@@ -113,11 +156,11 @@ class DetectionAlgorithmTab(qwt.QWidget):
 
         tab_wgt = qwt.QTabWidget()
 
-        input_picture_viewer = PictureViewer()
-        output_picture_viewer = PictureViewer()
+        self.input_picture_viewer = PictureViewer()
+        self.output_picture_viewer = PictureViewer()
 
-        tab_wgt.addTab(input_picture_viewer, 'Input faces')
-        tab_wgt.addTab(output_picture_viewer, 'Output faces')
+        tab_wgt.addTab(self.input_picture_viewer, 'Input faces')
+        tab_wgt.addTab(self.output_picture_viewer, 'Output faces')
 
         right_part_layout.addWidget(tab_wgt)
 
@@ -132,15 +175,97 @@ class DetectionAlgorithmTab(qwt.QWidget):
 
         self.setLayout(layout)
 
+    def add_picture_viewer_signals(self):
+        # message for input picture viewer
+        msg = Message(
+            MESSAGE_TYPE.REQUEST,
+            MESSAGE_STATUS.OK,
+            SIGNAL_OWNER.DETECTION_ALGORITHM_TAB,
+            SIGNAL_OWNER.MESSAGE_WORKER,
+            Body(
+                JOB_TYPE.ADD_SIGNAL,
+                {
+                    BODY_KEY.SIGNAL_OWNER:
+                    SIGNAL_OWNER.DETECTION_ALGORITHM_TAB_INPUT_PICTURE_VIEWER,
+                    BODY_KEY.SIGNAL: self.input_picture_added_sig,
+                }
+            )
+        )
+        self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(msg)
+
+        # message for output picture viewer
+        # msg.body.data[BODY_KEY.SIGNAL_OWNER] = \
+        #     SIGNAL_OWNER.DETECTION_ALGORITHM_TAB_OUTPUT_PICTURE_VIEWER
+        # msg.body.data[BODY_KEY.SIGNAL] = self.output_picture_added_sig,
+        # self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(msg)
+
+    @qtc.pyqtSlot(Message)
+    def input_picture_added(self, msg: Message):
+        data = msg.body.data
+        image = data[BODY_KEY.FILE]
+        self.input_picture_viewer.pictures_added_sig.emit([image])
+
+    @qtc.pyqtSlot(Message)
+    def output_picture_added(self, msg: Message):
+        data = msg.body.data
+        image = data[BODY_KEY.FILE]
+        self.output_picture_viewer.pictures_added_sig.emit([image])
+
     @qtc.pyqtSlot(int)
-    def face_detection_algorithm_selected(self, id: int):
+    def algorithm_selected(self, id: int):
         if id == -2:
-            self.algorithm_selected_sig.emit(
-                FACE_DETECTION_ALGORITHM.MTCNN)
+            self.algorithm_selected_value = FACE_DETECTION_ALGORITHM.MTCNN
         elif id == -3:
-            self.algorithm_selected_sig.emit(
-                FACE_DETECTION_ALGORITHM.FACEBOXES)
+            self.algorithm_selected_value = FACE_DETECTION_ALGORITHM.FACEBOXES
         else:
-            self.algorithm_selected_sig.emit(
-                FACE_DETECTION_ALGORITHM.S3FD)
-# self.start_detection_btn.setIcon(qtg.QIcon(qtg.QPixmap(':/play.svg')))
+            self.algorithm_selected_value = FACE_DETECTION_ALGORITHM.S3FD
+
+    def select_faces_directory(self, data_type: DATA_TYPE):
+        directory = qwt.QFileDialog.getExistingDirectory(
+            self, 'getExistingDirectory', './')
+        if directory:
+            if data_type == DATA_TYPE.INPUT:
+                self.input_faces_directory = directory
+            else:
+                self.output_faces_directory = directory
+
+            msg = Messages.CONSOLE_PRINT(
+                CONSOLE_MESSAGE_TYPE.LOG,
+                'Selected faces directory for ' +
+                f'{data_type.value.lower()}: {directory}.'
+            )
+
+        else:
+            msg = Messages.DIRECTORY_NOT_SELECTED()
+
+        self.signals[SIGNAL_OWNER.CONSOLE].emit(msg)
+
+    def select_model_path(self):
+        options = qwt.QFileDialog.Options()
+        options |= qwt.QFileDialog.DontUseNativeDialog
+        model_path, _ = qwt.QFileDialog.getOpenFileName(
+            self,
+            'Select model file',
+            "data/weights",
+            "Model file (*.pth)",
+            options=options)
+        if model_path:
+            self.model_path = model_path
+
+    def start_detection(self):
+        msg = Message(
+            MESSAGE_TYPE.REQUEST,
+            MESSAGE_STATUS.OK,
+            SIGNAL_OWNER.DETECTION_ALGORITHM_TAB,
+            SIGNAL_OWNER.FACE_DETECTION_WORKER,
+            Body(
+                JOB_TYPE.FACE_DETECTION,
+                {
+                    BODY_KEY.DATA_TYPE: DATA_TYPE.INPUT,
+                    BODY_KEY.INPUT_DATA_DIRECTORY: self.input_faces_directory,
+                    BODY_KEY.MODEL_PATH: self.model_path,
+                    BODY_KEY.ALGORITHM: self.algorithm_selected_value,
+                }
+            )
+        )
+        self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(msg)
