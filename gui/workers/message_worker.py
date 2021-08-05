1b06bb01c029dd3db6fb7aa62002220ea8162d34
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-05 18:23:13 +0200

    Funtionality for adding signals to message worker added.

--- gui/workers/message_worker.py
+++ gui/workers/message_worker.py
@@ -1,3 +1,4 @@
+from enums import BODY_KEY, JOB_TYPE, SIGNAL_OWNER
 from gui.workers.worker import Worker
 
 from message.message import Message
@@ -9,4 +10,12 @@ class MessageWorker(Worker):
         super().__init__(*args, **kwargs)
 
     def process(self, msg: Message):
-        self.signals[msg.recipient].emit(msg)
+
+        if msg.recipient == SIGNAL_OWNER.MESSAGE_WORKER:
+            if msg.body.job_type == JOB_TYPE.ADD_SIGNAL:
+                data = msg.body.data
+                signal_owner = data[BODY_KEY.SIGNAL_OWNER]
+                signal = data[BODY_KEY.SIGNAL]
+                self.add_signal(signal, signal_owner)
+        else:
+            self.signals[msg.recipient].emit(msg)
