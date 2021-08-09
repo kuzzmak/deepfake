11ea2a219ce78063361b6b73f7b77590ea1d5da3
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-09 21:17:08 +0200

    Refactore to new worker thread style.

--- gui/workers/threads/frames_extraction_worker_thread.py
+++ gui/workers/threads/frames_extraction_worker_thread.py
@@ -1,9 +1,31 @@
-from gui.workers.frames_extraction_worker import FramesExtractionWorker
+from enums import SIGNAL_OWNER
+from typing import Dict, Optional
 
+import PyQt5.QtCore as qtc
+
+from gui.workers.frames_extraction_worker import FramesExtractionWorker
 from gui.workers.threads.worker_thread import WorkerThread
 
 
 class FramesExtractionWorkerThread(WorkerThread):
+    """Thread used to split video into single frames.
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(FramesExtractionWorker(), *args, **kwargs)
+    Parameters
+    ----------
+    WorkerThread : WorkerThread
+        base worker thread class
+    """
+
+    def __init__(
+        self,
+        worker_signal: qtc.pyqtSignal,
+        signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict(),
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            FramesExtractionWorker(signals),
+            worker_signal,
+            *args,
+            **kwargs
+        )
