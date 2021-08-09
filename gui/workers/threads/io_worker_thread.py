018b8a3190437e4fd18d9b78068eae12a16bffd7
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-09 20:56:13 +0200

    Adapted to new style.

--- gui/workers/threads/io_worker_thread.py
+++ gui/workers/threads/io_worker_thread.py
@@ -1,8 +1,32 @@
+from enums import SIGNAL_OWNER
+from typing import Dict, Optional
+
+import PyQt5.QtCore as qtc
+
 from gui.workers.io_worker import IO_Worker
 from gui.workers.threads.worker_thread import WorkerThread
 
 
 class IO_WorkerThread(WorkerThread):
+    """Worker for doing any kind of IO operation: saveing, renaming,
+    deleting files.
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(IO_Worker(), *args, **kwargs)
+    Parameters
+    ----------
+    WorkerThread : WorkerThread
+        base worker thread class
+    """
+
+    def __init__(
+        self,
+        worker_signal: qtc.pyqtSignal,
+        signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict(),
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            IO_Worker(signals),
+            worker_signal,
+            *args,
+            **kwargs
+        )
