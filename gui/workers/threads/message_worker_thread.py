5bb3bb5c792fadb64636342abbe9f08875d93266
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-09 20:49:17 +0200

    Refactored to new style where all signals and worker signal is passed on init.

--- gui/workers/threads/message_worker_thread.py
+++ gui/workers/threads/message_worker_thread.py
@@ -1,8 +1,24 @@
+from enums import SIGNAL_OWNER
+from typing import Dict, Optional
+
+import PyQt5.QtCore as qtc
+
 from gui.workers.threads.worker_thread import WorkerThread
 from gui.workers.message_worker import MessageWorker
 
 
 class MessageWorkerThread(WorkerThread):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(MessageWorker(), *args, **kwargs)
+    def __init__(
+        self,
+        worker_signal: qtc.pyqtSignal,
+        signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict(),
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            MessageWorker(signals),
+            worker_signal,
+            *args,
+            **kwargs
+        )
