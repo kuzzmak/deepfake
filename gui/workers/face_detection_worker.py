6b0873cd2e5e78b184aca4ab32a22a421a218d79
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-11 00:38:37 +0200

    Refactoring, faces from input images are shown in picture viewer and saved to disk.

--- gui/workers/face_detection_worker.py
+++ gui/workers/face_detection_worker.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, List, Optional
 
 import PyQt5.QtCore as qtc
@@ -8,15 +9,17 @@ import numpy as np
 
 import cv2 as cv
 
-from PIL import Image
-
 from enums import (
     BODY_KEY,
+    DEVICE,
     FACE_DETECTION_ALGORITHM,
+    FILE_TYPE,
+    IO_OPERATION_TYPE,
     JOB_TYPE,
     MESSAGE_STATUS,
     MESSAGE_TYPE,
     SIGNAL_OWNER,
+    WIDGET,
 )
 
 from core.face_detection.algorithms.s3fd.data.config import cfg
@@ -25,16 +28,9 @@ from core.face_detection.algorithms.s3fd.utils.augmentations import to_chw_bgr
 
 from gui.workers.worker import Worker
 
-from message.message import Body, Message
+from message.message import Body, IOOperationBody, Message, Messages
 
 from utils import get_file_paths_from_dir
-
-use_cuda = torch.cuda.is_available()
-
-if use_cuda:
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-else:
-    torch.set_default_tensor_type('torch.FloatTensor')
 
 
 class FaceDetectionWorker(Worker):
@@ -56,46 +52,90 @@ class FaceDetectionWorker(Worker):
         output_faces_directory = data[BODY_KEY.OUTPUT_FACES_DIRECTORY]
         algorithm = data[BODY_KEY.ALGORITHM]
         model_path = data[BODY_KEY.MODEL_PATH]
+        device = data[BODY_KEY.DEVICE]
 
-        input_images = get_file_paths_from_dir(input_data_directory)
+        if device == DEVICE.CUDA:
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        else:
+            torch.set_default_tensor_type('torch.FloatTensor')
 
         if algorithm == FACE_DETECTION_ALGORITHM.S3FD:
-            net = build_s3fd('test', cfg.NUM_CLASSES)
+            net = build_s3fd('test')
             net.load_state_dict(
                 torch.load(
                     model_path,
-                    map_location=torch.device('cpu')
+                    map_location=torch.device(device.value)
                 )
             )
             net.eval()
 
-        for img_path in input_images:
+        if input_data_directory is not None:
+            input_images = get_file_paths_from_dir(input_data_directory)
 
-            extraced_faces = detect(net, img_path, 0.6)
+            _msg = Messages.CONFIGURE_WIDGET(
+                SIGNAL_OWNER.FACE_DETECTION_WORKER,
+                WIDGET.JOB_PROGRESS,
+                'setMaximum',
+                [len(input_images)],
+            )
+            self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(_msg)
 
-            for face in extraced_faces:
-                msg = Message(
-                    MESSAGE_TYPE.REQUEST,
-                    MESSAGE_STATUS.OK,
-                    SIGNAL_OWNER.FACE_DETECTION_WORKER,
-                    SIGNAL_OWNER.DETECTION_ALGORITHM_TAB_INPUT_PICTURE_VIEWER,
-                    Body(
-                        JOB_TYPE.IMAGE_DISPLAY,
-                        {
-                            BODY_KEY.FILE: face,
-                        }
+            input_images_counter = 0
+            for img_path in input_images:
+
+                faces = detect(net, img_path, 0.6, device)
+
+                for face in faces:
+                    _msg = Message(
+                        MESSAGE_TYPE.REQUEST,
+                        MESSAGE_STATUS.OK,
+                        SIGNAL_OWNER.FACE_DETECTION_WORKER,
+                        SIGNAL_OWNER.DETECTION_ALGORITHM_TAB_INPUT_PICTURE_VIEWER,
+                        Body(
+                            JOB_TYPE.IMAGE_DISPLAY,
+                            {
+                                BODY_KEY.FILE: face,
+                            }
+                        )
                     )
-                )
-                self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(msg)
+                    self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(_msg)
+
+                    _msg = Message(
+                        MESSAGE_TYPE.REQUEST,
+                        MESSAGE_STATUS.OK,
+                        SIGNAL_OWNER.FRAMES_EXTRACTION_WORKER,
+                        SIGNAL_OWNER.IO_WORKER,
+                        IOOperationBody(
+                            io_operation_type=IO_OPERATION_TYPE.SAVE,
+                            file_path=os.path.join(
+                                input_faces_directory,
+                                f'if_{input_images_counter}.png',
+                            ),
+                            file=face,
+                            file_type=FILE_TYPE.IMAGE,
+                            multipart=True,
+                            part=input_images_counter + 1,
+                            total=len(input_images),
+                        )
+                    )
+                    self.signals[SIGNAL_OWNER.MESSAGE_WORKER].emit(_msg)
+
+        if output_data_directory is not None:
+            output_images = get_file_paths_from_dir(output_data_directory)
 
 
-def detect(net, img_path: str, thresh: float):
+def detect(net, img_path: str, thresh: float, device: DEVICE):
     img = cv.imread(img_path, cv.IMREAD_COLOR)
     height, width, _ = img.shape
-    max_im_shrink = np.sqrt(
-        1700 * 1200 / (img.shape[0] * img.shape[1]))
-    image = cv.resize(img, None, None, fx=max_im_shrink,
-                      fy=max_im_shrink, interpolation=cv.INTER_LINEAR)
+    max_im_shrink = np.sqrt(1700 * 1200 / (height * width))
+    image = cv.resize(
+        img,
+        None,
+        None,
+        fx=max_im_shrink,
+        fy=max_im_shrink,
+        interpolation=cv.INTER_LINEAR,
+    )
     # image = cv2.resize(img, (640, 640))
     x = to_chw_bgr(image)
     x = x.astype('float32')
@@ -103,14 +143,12 @@ def detect(net, img_path: str, thresh: float):
     x = x[[2, 1, 0], :, :]
 
     x = torch.autograd.Variable(torch.from_numpy(x).unsqueeze(0))
-    if use_cuda:
+    if device == DEVICE.CUDA:
         x = x.cuda()
 
     y = net(x)
     detections = y.data
     scale = torch.Tensor([width, height, width, height])
-
-    img = cv.imread(img_path, cv.IMREAD_COLOR)
 
     faces = []
 
@@ -119,7 +157,7 @@ def detect(net, img_path: str, thresh: float):
         while detections[0, i, j, 0] >= thresh:
             pt = (detections[0, i, j, 1:] * scale).cpu().numpy()
             j += 1
-            faces.append((int(pt[0]), int(pt[1]), int(pt[2])), int(pt[3]))
+            faces.append((int(pt[0]), int(pt[1]), int(pt[2]), int(pt[3])))
 
     return extract_faces(faces, img)
 
