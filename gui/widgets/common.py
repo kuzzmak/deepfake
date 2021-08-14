ed102d110df0d03fc4e6e286d316336c7a813405
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-14 16:22:02 +0200

    File for common widgets.

---
+++ gui/widgets/common.py
@@ -0,0 +1,16 @@
+import PyQt5.QtWidgets as qwt
+
+
+VerticalSpacer = qwt.QSpacerItem(
+    1,
+    1,
+    qwt.QSizePolicy.Fixed,
+    qwt.QSizePolicy.MinimumExpanding,
+)
+
+HorizontalSpacer = qwt.QSpacerItem(
+    1,
+    1,
+    qwt.QSizePolicy.MinimumExpanding,
+    qwt.QSizePolicy.Fixed,
+)
