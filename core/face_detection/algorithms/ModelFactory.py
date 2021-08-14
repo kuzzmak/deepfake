a51c792bbc1daf98dee6dbd9d1efb54f1d7b5e6a
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-14 18:32:14 +0200

    Base class for face detection factories.

---
+++ core/face_detection/algorithms/ModelFactory.py
@@ -0,0 +1,26 @@
+import abc
+from enums import DEVICE
+
+import torch.nn as nn
+
+
+class ModelFactory(abc.ABCMeta):
+    """Base class which every model factory should implement.
+    """
+
+    @abc.abstractstaticmethod
+    def build_model(device: DEVICE) -> nn.Module:
+        """Builds specific model. Loads weights of the model and
+        puts model in eval mode.
+
+        Parameters
+        ----------
+        device : DEVICE
+            device where computation should be done
+
+        Returns
+        -------
+        nn.Module
+            model
+        """
+        ...
