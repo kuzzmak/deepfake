753a2f3f665d7a717e7acdcf17c90d31535d7e20
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-16 13:14:24 +0200

    Model loaded from google disk if it doesn't exist in torch.hub folder.

--- core/face_detection/algorithms/faceboxes/faceboxes_model_factory.py
+++ core/face_detection/algorithms/faceboxes/faceboxes_model_factory.py
@@ -8,17 +8,19 @@ from core.face_detection.algorithms.ModelFactory import ModelFactory
 
 from enums import DEVICE
 
+from utils import load_file_from_google_drive
+
 
 class FaceboxesModelFactory(ModelFactory):
 
     def build_model(device: DEVICE) -> nn.Module:
         net = FaceBoxes()
-        model_path = APP_CONFIG.app.core.face_detection.algorithms.faceboxes.weight_path
+        model_id = APP_CONFIG.app.core.face_detection.algorithms.faceboxes.gd_id
+        dict_path = load_file_from_google_drive(model_id, 'faceboxes.pth')
         net.load_state_dict(
             torch.load(
-                model_path,
+                dict_path,
                 map_location=torch.device(device.value)
             )
         )
-        net.eval()
         return net
