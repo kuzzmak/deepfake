dac2322b799b764eee81384485682829ec93b6c3
kuzzmak <kuzminski.antonio@gmail.com>
2021-08-25 13:25:12 +0200

    Class for everything that has to do with face, landmark and mask extraction from images.

--- core/extractor.py
+++ core/extractor.py
@@ -1,8 +1,12 @@
-import numpy as np
 from typing import List
 
+import numpy as np
+
 from core.face import Face
-from core.face_detection.algorithms.FaceDetectionModel \
+from core.face_alignment.face_aligner import FaceAligner
+from core.landmark_detection.landmark_detection_model \
+    import LandmarkDetectionModel
+from core.face_detection.algorithms.face_detection_model \
     import FaceDetectionModel
 
 
@@ -11,13 +15,80 @@ class Extractor:
     def __init__(
         self,
         fdm: FaceDetectionModel,
-        # ldm: LandmarkDetectionModel,
+        ldm: LandmarkDetectionModel,
     ) -> None:
         self.fdm = fdm
+        self.ldm = ldm
 
     def detect_faces(self, image: np.ndarray) -> List[Face]:
-        faces = self.fdm.detect(image)
+        """Initiates face detection process on the `image`. When face is
+        detected, `Face` object is created and followind properties are
+        set:
+            - `raw_image` which is image from the input
+            - `bounding_box` which bounds face with two dots, upper left
+                and lower right and they make a bounding rectangle
+
+        Parameters
+        ----------
+        image : np.ndarray
+            image with potential faces
+
+        Returns
+        -------
+        List[Face]
+            list of detected `Face` objects
+        """
+        faces = self.fdm.detect_faces(image)
         return faces
 
-    def detect_landmarks(self):
-        ...
+    def detect_landmarks(self, face: Face) -> None:
+        """Initiates process of face landmark detection on the `face` object.
+        After detection is done `landmarks` property is set on the `face`
+        object.
+
+        Note: face detection prior to this process has to be done in order to
+        obtain bounding boxes of the potential faces.
+
+        Parameters
+        ----------
+        face : Face
+            face object containing bounding boxes
+        """
+        landmarks = self.ldm.detect_landmarks(face)
+        face.landmarks = landmarks
+
+    def get_aligned_face(self, face: Face, image_size: int) -> np.ndarray:
+        """Aligned and centered face getter.
+
+        Parameters
+        ----------
+        face : Face
+            face object containing everything necessary
+        image_size : int
+            size of the square in which image will be resized
+
+        Returns
+        -------
+        np.ndarray
+            aligned face
+        """
+        aligned_face = FaceAligner.get_aligned_face(face, image_size)
+        return aligned_face
+
+    def get_aligned_mask(self, face: Face, image_size: int) -> np.ndarray:
+        """Aligned and centered face mask getter.
+
+        Parameters
+        ----------
+        face : Face
+            face object containing everything necessary
+        image_size : int
+            size of the square in which image will be resized
+
+        Returns
+        -------
+        np.ndarray
+            aligned face mask
+        """
+        aligned_mask = FaceAligner.get_aligned_mask(face, image_size)
+        return aligned_mask
