79adc0929e7a5ef790a56f335f0c6224be2e6726
kuzzmak <kuzminski.antonio@gmail.com>
2021-09-07 23:48:20 +0200

    On app start, logging is configured.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from app import App
+import logging
+import logging.config
+import yaml
 
+from app import App
+import config
 
 if __name__ == '__main__':
+
+    with open('logging_config.yaml', 'r') as f:
+        _conf = yaml.safe_load(f.read())
+        logging.config.dictConfig(_conf)
+
+    config.refresh_config()
+
     app = App()
     app.gui()
