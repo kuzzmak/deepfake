4fa5bb8a801ba6f7ba0a54b11d74130b7bf38cfd
kuzzmak <kuzminski.antonio@gmail.com>
2021-09-19 20:29:00 +0200

    Changed notations on tensor shapes, changed order of imports.

--- core/model/original_ae.py
+++ core/model/original_ae.py
@@ -1,11 +1,10 @@
 from typing import Tuple
 
 import torch
-import torch.nn as nn
 
-from core.model.ae_model import DeepfakeAEModel
-from core.layer import Conv2d, Linear, Flatten, ResBlock, Upscale
 from core.activation import LeakyReLu, Sigmoid
+from core.layer import Conv2d, Linear, Flatten, ResBlock, Upscale
+from core.model.ae_model import DeepfakeAEModel
 
 
 class OriginalAE(DeepfakeAEModel):
@@ -63,7 +62,9 @@ class OriginalAE(DeepfakeAEModel):
         # calculate size of the input in linear layer
         last_conv_filters = self.encoder_channels[-1][1]
         linear_in_features = self._calculate_linar_input_size(
-            last_conv_filters
+            last_conv_filters,
+            5,
+            2,
         )
 
         self.flatten = Flatten()
@@ -162,36 +163,41 @@ class OriginalAE(DeepfakeAEModel):
             in_ch, out_ch = channels
             conv = getattr(self, self.conv_template.format(in_ch, out_ch))
             x = conv(x)
-        x = self.flatten(x)                     # [1, 719872]
-        x = self.linear_1024(x)                 # [1, 1024]
-        x = self.linear_16384(x)                # [1, 16384]
-        x = torch.reshape(x, (-1, 1024, 4, 4))  # [1, 1024, 4, 4]
-        x = self.upscale_512_e(x)               # [1, 512, 8, 8]
+        x = self.flatten(x)                     # [bs, linear_in_features]
+        x = self.linear_1024(x)                 # [bs, 1024]
+        x = self.linear_16384(x)                # [bs, 16384]
+        x = torch.reshape(x, (-1, 1024, 4, 4))  # [bs, 1024, 4, 4]
+        x = self.upscale_512_e(x)               # [bs, 512, 8, 8]
         return x
 
     def decoder(self, x: torch.Tensor) -> torch.Tensor:
-        x1 = self.upscale_512_d_1(x)            # [1, 512, 16, 16]
-        x1 = self.res_block_512_d_1(x1)         # [1, 512, 16, 16]
-        x1 = self.upscale_256_d_1(x1)           # [1, 256, 32, 32]
-        x1 = self.res_block_256_d_1(x1)         # [1, 256, 32, 32]
-        x1 = self.upscale_128_d_1(x1)           # [1, 128, 64, 64]
-        x1 = self.res_block_128_d_1(x1)         # [1, 128, 64, 64]
-        x1 = self.upscale_64_d_1(x1)            # [1, 64, 128, 128]
-        x1 = self.conv_d_1(x1)                  # [1, 3, 128, 128]
+        x1 = self.upscale_512_d_1(x)            # [bs, 512, 16, 16]
+        x1 = self.res_block_512_d_1(x1)         # [bs, 512, 16, 16]
+        x1 = self.upscale_256_d_1(x1)           # [bs, 256, 32, 32]
+        x1 = self.res_block_256_d_1(x1)         # [bs, 256, 32, 32]
+        x1 = self.upscale_128_d_1(x1)           # [bs, 128, 64, 64]
+        x1 = self.res_block_128_d_1(x1)         # [bs, 128, 64, 64]
+        x1 = self.upscale_64_d_1(x1)            # [bs, 64, 128, 128]
+        x1 = self.conv_d_1(x1)                  # [bs, 3, 128, 128]
 
-        x2 = self.upscale_512_d_2(x)            # [1, 512, 16, 16]
-        x2 = self.upscale_256_d_2(x2)           # [1, 256, 32, 32]
-        x2 = self.upscale_128_d_2(x2)           # [1, 128, 64, 64]
-        x2 = self.upscale_64_d_2(x2)            # [1, 64, 128, 128]
-        x2 = self.conv_d_2(x2)                  # [1, 1, 128, 128]
+        x2 = self.upscale_512_d_2(x)            # [bs, 512, 16, 16]
+        x2 = self.upscale_256_d_2(x2)           # [bs, 256, 32, 32]
+        x2 = self.upscale_128_d_2(x2)           # [bs, 128, 64, 64]
+        x2 = self.upscale_64_d_2(x2)            # [bs, 64, 128, 128]
+        x2 = self.conv_d_2(x2)                  # [bs, 1, 128, 128]
 
         return x1, x2
 
-    def _calculate_linar_input_size(self, last_conv_filters: int):
+    def _calculate_linar_input_size(
+        self,
+        last_conv_filters: int,
+        kernel: int,
+        stride: int,
+    ):
         shape = [self.input_shape[1], self.input_shape[2]]
         for _ in range(len(self.encoder_channels)):
-            w = int((shape[1] - 5) / 2 + 1)
-            h = int((shape[0] - 5) / 2 + 1)
+            w = int((shape[1] - kernel) / stride + 1)
+            h = int((shape[0] - kernel) / stride + 1)
             shape[0] = h
             shape[1] = w
         return shape[0] * shape[1] * last_conv_filters
