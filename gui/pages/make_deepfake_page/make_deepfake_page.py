fa0d7e339d0232022d7ec6904c57ffac08fb87dd
kuzzmak <kuzminski.antonio@gmail.com>
2021-10-03 21:12:29 +0200

    Removed whitespace.

--- gui/pages/make_deepfake_page/make_deepfake_page.py
+++ gui/pages/make_deepfake_page/make_deepfake_page.py
@@ -37,7 +37,7 @@ class MakeDeepfakePage(Page):
         self,
         parent,
         signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = dict(),
-        * args,
+        *args,
         **kwargs,
     ):
         super().__init__(
