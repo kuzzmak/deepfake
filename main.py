8eeba35bebf7f59e73c21f1aab5c2290ae6f58af
kuzzmak <kuzminski.antonio@gmail.com>
2021-10-07 00:10:19 +0200

    Ignoring logs from torch.ignite.engine.

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
 
     with open('logging_config.yaml', 'r') as f:
         _conf = yaml.safe_load(f.read())
-        logging.config.dictConfig(_conf)
-
+    logging.config.dictConfig(_conf)
+    logging.getLogger("ignite.engine.engine.Engine").setLevel(logging.WARNING)
     config.refresh_config()
 
     app = App()
