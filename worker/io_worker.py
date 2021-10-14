f21283e8aabc882f5238b5f28bbaec41535b0d06
kuzzmak <kuzminski.antonio@gmail.com>
2021-10-14 19:15:44 +0200

    IO worker class for faces metadata.

---
+++ worker/io_worker.py
@@ -0,0 +1,29 @@
+import logging
+from typing import List
+
+import PyQt5.QtCore as qtc
+
+from core.face import Face
+from serializer.face_serializer import FaceSerializer
+
+logger = logging.getLogger(__name__)
+
+
+class Worker(qtc.QObject):
+
+    finished = qtc.pyqtSignal()
+
+    def __init__(self, faces: List[Face], metadata_path: str) -> None:
+        super().__init__()
+        self.faces = faces
+        self.metadata_path = metadata_path
+
+    def run(self):
+        logger.info(
+            f'Saving {len(self.faces)} faces metadata objects. ' +
+            'Please wait.'
+        )
+        for face in self.faces:
+            FaceSerializer.save(face, self.metadata_path)
+        self.finished.emit()
+        logger.info('Finished saving.')
