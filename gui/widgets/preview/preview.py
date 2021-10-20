7df8b537fe8eed1336b22ad5cc6e8da9690aa3a3
kuzzmak <kuzminski.antonio@gmail.com>
2021-10-21 00:35:34 +0200

    Updated preview canvas: now Face A, A->A, A->B, Face B, B->A and B->B are shown in preview.

--- gui/widgets/preview/preview.py
+++ gui/widgets/preview/preview.py
@@ -2,6 +2,7 @@ import sys
 import traceback
 from typing import List
 
+from matplotlib.axis import Axis
 from matplotlib.animation import TimedAnimation
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
@@ -19,7 +20,7 @@ class CustomFigCanvas(FigureCanvasQTAgg, TimedAnimation):
 
         self._new_data: List[torch.Tensor] = []
 
-        subplot_titles = ['input', 'AA']
+        subplot_titles = ['Face A', 'A->A', 'A->B', 'Face B', 'B->A', 'B->B']
         self.n_images = n_images
         self.n_cols = len(subplot_titles)
 
@@ -34,9 +35,14 @@ class CustomFigCanvas(FigureCanvasQTAgg, TimedAnimation):
         TimedAnimation.__init__(self, self.fig, interval=200, blit=True)
 
     @staticmethod
-    def _hide_axes(axe):
-        axe.axes.get_xaxis().set_visible(False)
-        axe.axes.get_yaxis().set_visible(False)
+    def _hide_axes(axis: Axis):
+        """Hides axes and axe values from every single image displayed.
+
+        Args:
+            axis (Axis): axis which axes should be hidden
+        """
+        axis.axes.get_xaxis().set_visible(False)
+        axis.axes.get_yaxis().set_visible(False)
 
     def new_frame_seq(self):
         return iter(range(self.n_images))
@@ -51,23 +57,21 @@ class CustomFigCanvas(FigureCanvasQTAgg, TimedAnimation):
 
     def _draw_frame(self, framedata):
         while len(self._new_data) > 0:
+            # get oldest data sent to preview widget
             images = self._new_data[0]
+            # iterate through every row
             for row in range(self.n_images):
-                axe_input = self.axes[row * self.n_cols]
-                try:
-                    img_input = tensor_to_np_image(images[0][row])
-                    axe_input.imshow(img_input)
-                except IndexError:
-                    # happens when one batch doesn't contain enough images to
-                    # display
-                    ...
-
-                axe_output = self.axes[row * self.n_cols + 1]
-                try:
-                    img_output = tensor_to_np_image(images[1][row])
-                    axe_output.imshow(img_output)
-                except IndexError:
-                    ...
+                # through every column
+                for col in range(self.n_cols):
+                    # get corresponding axe from an array
+                    axe = self.axes[row * self.n_cols + col]
+                    try:
+                        # convert tensor from model or model input to "normal"
+                        # image which can be displayed in matplotlib canvas
+                        img_input = tensor_to_np_image(images[col][row])
+                        axe.imshow(img_input)
+                    except IndexError:
+                        ...
             del self._new_data[0]
 
     def add_data(self, data: List[torch.Tensor]):
@@ -79,6 +83,11 @@ class Preview(BaseWidget):
     refresh_data_sig = qtc.pyqtSignal(list)
 
     def __init__(self):
+        """Vidget used to display progress of the training process. It
+        includes original face A, face A through decoder A, face A through
+        decoder B, original face B, face B through decoder A and face B
+        through decoder B.
+        """
         super().__init__()
         self._init_ui()
         self.refresh_data_sig.connect(self._refresh_data)
