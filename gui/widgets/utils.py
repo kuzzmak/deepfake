d2e12f3fc361605cad47a07f7700e6eb917d341a
kuzzmak <kuzminski.antonio@gmail.com>
2021-11-05 10:08:52 +0100

    Function for widget coloring.

---
+++ gui/widgets/utils.py
@@ -0,0 +1,20 @@
+import PyQt5.QtCore as qtc
+import PyQt5.QtWidgets as qwt
+
+
+def set_color_on_widget(
+    widget: qwt.QWidget,
+    color: qtc.Qt.GlobalColor = qtc.Qt.red,
+):
+    """Sets some color on the widget. Can be used for styling or debugging
+    purposes where true widget size can be seen only when color is applied.
+
+    Args:
+        widget (qwt.QWidget): widget which color is about to change
+        color (qtc.Qt.GlobalColor, optional): paint widget to which color.
+            Defaults to qtc.Qt.red.
+    """
+    widget.setAutoFillBackground(True)
+    p = widget.palette()
+    p.setColor(widget.backgroundRole(), color)
+    widget.setPalette(p)
