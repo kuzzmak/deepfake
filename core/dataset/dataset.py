cd437a403ac811df58ca8d9c1f1c5ed305c75547
kuzzmak <kuzminski.antonio@gmail.com>
2021-11-07 21:33:57 +0100

    Added image augmentations to the dataset, added function which should augment every single image in batch, not imeplemented yet.

--- core/dataset/dataset.py
+++ core/dataset/dataset.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional, Tuple
+from typing import Callable, List, Optional, Tuple
 
 import numpy as np
 import torch
@@ -28,6 +28,7 @@ class DeepfakeDataset(Dataset):
         metadata_path_A: str,
         metadata_path_B: str,
         input_shape: int,
+        image_augmentations: List[Callable] = [],
         load_into_memory: bool = False,
         device: DEVICE = DEVICE.CPU,
         transforms: Optional[transforms.Compose] = None,
@@ -42,6 +43,8 @@ class DeepfakeDataset(Dataset):
             path of the `Faces` metadata of person B
         input_shape : int
             size of the square to which face and mask are resized
+        image_augmentations : List[Callable]
+            list of functions for doing augmentations on image
         load_into_memory : bool, optional
             should dataset be loaded into memory, by default False
         device : DEVICE, optional
@@ -52,6 +55,7 @@ class DeepfakeDataset(Dataset):
         self.input_shape = input_shape
         self.load_into_memory = load_into_memory
         self.device = device
+        self.image_augmentations = image_augmentations
         self.transforms = transforms if transforms is not None \
             else default_transforms
         self.metadata_paths_A = get_file_paths_from_dir(metadata_path_A, ['p'])
@@ -72,12 +76,12 @@ class DeepfakeDataset(Dataset):
         )
         for path in self.metadata_paths_A:
             face_A, mask_A = self._load_from_path(path)
-            self.faces_A.append(face_A.to(self.device.value))
-            self.masks_A.append(mask_A.to(self.device.value))
+            self.faces_A.append(face_A)
+            self.masks_A.append(mask_A)
         for path in self.metadata_paths_B:
             face_B, mask_B = self._load_from_path(path)
-            self.faces_B.append(face_B.to(self.device.value))
-            self.masks_B.append(mask_B.to(self.device.value))
+            self.faces_B.append(face_B)
+            self.masks_B.append(mask_B)
         logger.info(
             f'Loaded {len(self.faces_A)} face_A metadata ' +
             f'and {len(self.faces_B)} face_B metadata into memory.'
@@ -125,6 +129,21 @@ class DeepfakeDataset(Dataset):
         aligned_mask = FaceAligner.get_aligned_mask(face, self.input_shape)
         return aligned_face, aligned_mask
 
+    def _augment(self, image: np.ndarray) -> np.ndarray:
+        """Augments every image in the batch based on the image augmentation
+        function and parameters user chose.
+
+        Args:
+            image (np.ndarray): image to be augmented
+
+        Returns:
+            np.ndarray: augmented image if augmentations were chosen, normal
+                image otherwise
+        """
+        # for aug in self.image_augmentations:
+        #     image = aug(image)
+        return image
+
     def _transform(
         self,
         face: np.ndarray,
@@ -155,15 +174,22 @@ class DeepfakeDataset(Dataset):
     def __getitem__(
         self,
         index: int,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    ) -> Tuple[
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+    ]:
         if self.load_into_memory:
             # load from memory
             return (
-                self.faces_A[index],
+                self._augment(self.faces_A[index]),
                 self.faces_A[index],
                 self.masks_A[index],
 
-                self.faces_B[index],
+                self._augment(self.faces_B[index]),
                 self.faces_B[index],
                 self.masks_B[index],
             )
@@ -172,4 +198,11 @@ class DeepfakeDataset(Dataset):
         path_B = self.metadata_paths_B[index]
         face_A, mask_A = self._load_from_path(path_A)
         face_B, mask_B = self._load_from_path(path_B)
-        return face_A, face_A, mask_A, face_B, face_B, mask_B
+        return (
+            self._augment(face_A),
+            face_A,
+            mask_A,
+            self._augment(face_B),
+            face_B,
+            mask_B,
+        )
