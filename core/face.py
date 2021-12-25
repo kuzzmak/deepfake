56a8945d1a38055f4727e1c011bf352b1a9c691b
kuzzmak <kuzminski.antonio@gmail.com>
2021-12-25 17:29:28 +0100

    Refactored face class for easier usage.

--- core/face.py
+++ core/face.py
@@ -1,5 +1,6 @@
-import cv2 as cv
+from typing import Union
 
+import cv2 as cv
 import numpy as np
 
 from core.bounding_box import BoundingBox
@@ -10,60 +11,109 @@ from core.landmarks import Landmarks
 
 class Face:
     """Class for everything that has to do with faces. Contains image from
-    which faces are extracted, extracted faces, alignments... Also, all
-    operations that should be made on faces, should be implemented here.
+    which faces are extracted, extracted faces, alignments...
     """
 
-    def __init__(
-        self,
-        raw_image: Image = None,
-        bounding_box: BoundingBox = None,
-        detected_face: np.ndarray = None,
-        landmarks: Landmarks = None,
-        alignment: np.ndarray = None,
-    ):
-        """Constructor.
-
-        Parameters
-        ----------
-        raw_image : Image, optional
-            image object containing image data and other properties,
-            by default None
-        bounding_box : BoundingBox, optional
-            two dots on the image which are enough to make a bounding box
-            containing detected face, by default None
-        detected_face : np.ndarray, optional
-            detected face in the raw_image, by default None
-        landmarks : Landmarks, optional
-            object containing face landmark dots, by default None
-        alignment : np.ndarray, optional
-            alignment which transforms initial image to the mean face
-        """
-        self._raw_image = raw_image
-        self._bounding_box = bounding_box
-        self._detected_face = detected_face
-        self._landmarks = landmarks
-        self._alignment = alignment
+    def __init__(self):
+        self._raw_image = None
+        self._bounding_box = None
+        self._detected_face = None
+        self._landmarks = None
+        self._alignment = None
+        self._aligned_landmarks = None
+        self._aligned_image = None
+        self._aligned_mask = None
 
     @property
-    def raw_image(self) -> Image:
+    def raw_image(self) -> Union[Image, None]:
+        """Raw image on which was detected face.
+
+        Returns
+        -------
+        Union[Image, None]
+            raw image if it was set, else None
+        """
         return self._raw_image
 
     @property
-    def bounding_box(self) -> BoundingBox:
+    def bounding_box(self) -> Union[BoundingBox, None]:
+        """Bounding box around the face which is representd by two points.
+
+        Returns
+        -------
+        Union[BoundingBox, None]
+            bounding box if the face detection process was run, None else
+        """
         return self._bounding_box
 
     @property
-    def detected_face(self) -> np.ndarray:
+    def detected_face(self) -> Union[np.ndarray, None]:
+        """Detected face by the face detection process.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            detected face if the face detection process was run, None else
+        """
         return self._detected_face
 
     @property
-    def landmarks(self) -> Landmarks:
+    def landmarks(self) -> Union[Landmarks, None]:
+        """Detected 68 face points.
+
+        Returns
+        -------
+        Union[Landmarks, None]
+            landmarks if the landmark detection process was run, None else
+        """
         return self._landmarks
 
     @property
-    def alignment(self) -> np.ndarray:
+    def alignment(self) -> Union[np.ndarray, None]:
+        """Alignment matrix which transforms this face into a mean face.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            alignment matrix if the alignment process was run, None else
+        """
         return self._alignment
+
+    @property
+    def aligned_landmarks(self) -> Union[np.ndarray, None]:
+        """Landmarks which were aligned to the aligned image based on the
+        `alignment`.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            aligned landmarks if the alignment process was run, None else
+        """
+        return self._aligned_landmarks
+
+    @property
+    def aligned_image(self) -> Union[np.ndarray, None]:
+        """Aligned and resized raw image to the mean face based on the
+        `alignment`.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            aligned image if the alignment process was run, None else
+        """
+        return self._aligned_image
+
+    @property
+    def aligned_mask(self) -> Union[np.ndarray, None]:
+        """Aligned and resized mask to the mean face based on the
+        `alignment`. Contains ones on the places where's subjects face.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            aligned mask if the alignment process was run, None else
+        """
+        return self._aligned_mask
 
     @raw_image.setter
     def raw_image(self, raw_image: Image):
@@ -84,6 +134,22 @@ class Face:
     @alignment.setter
     def alignment(self, alignment: np.ndarray):
         self._alignment = alignment
+
+    @aligned_landmarks.setter
+    def aligned_landmarks(self, aligned_landmarks: np.ndarray):
+        self._aligned_landmarks = aligned_landmarks
+
+    @aligned_image.setter
+    def aligned_image(self, aligned_image: np.ndarray) -> np.ndarray:
+        self._aligned_image = aligned_image
+
+    @aligned_mask.setter
+    def aligned_mask(self, aligned_mask: np.ndarray) -> np.ndarray:
+        self._aligned_mask = aligned_mask
+
+    def masked_face_image(self, aligned: bool = True) -> np.ndarray:
+        out = self.aligned_image * self.aligned_mask[..., None]
+        return out
 
     def draw_landmarks(self) -> np.ndarray:
         """Draws small dots which represent face landmarks on the raw image
