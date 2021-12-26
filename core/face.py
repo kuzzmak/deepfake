da1114ec27a50af5e40e2567b208b02e6f6b662a
kuzzmak <kuzminski.antonio@gmail.com>
2021-12-26 20:47:06 +0100

    Changed how mask property is made, added ability to show only face on raw and aligned image.

--- core/face.py
+++ core/face.py
@@ -16,6 +16,7 @@ class Face:
 
     def __init__(self):
         self._raw_image = None
+        self._mask = None
         self._bounding_box = None
         self._detected_face = None
         self._landmarks = None
@@ -34,6 +35,19 @@ class Face:
             raw image if it was set, else None
         """
         return self._raw_image
+
+    @property
+    def mask(self) -> Union[np.ndarray, None]:
+        """Binary mask which multiplied with the raw image only gives image
+        where only face is visible without background i.e. convex polygon
+        defined by landmarks is only visible on raw image.
+
+        Returns
+        -------
+        Union[np.ndarray, None]
+            binary mask for raw image
+        """
+        return self._mask
 
     @property
     def bounding_box(self) -> Union[BoundingBox, None]:
@@ -119,6 +133,10 @@ class Face:
     def raw_image(self, raw_image: Image):
         self._raw_image = raw_image
 
+    @mask.setter
+    def mask(self, mask: np.ndarray):
+        self._mask = mask
+
     @bounding_box.setter
     def bounding_box(self, bounding_box: BoundingBox):
         self._bounding_box = bounding_box
@@ -148,8 +166,26 @@ class Face:
         self._aligned_mask = aligned_mask
 
     def masked_face_image(self, aligned: bool = True) -> np.ndarray:
-        out = self.aligned_image * self.aligned_mask[..., None]
-        return out
+        """Multiplies aligned image with the aligned mask and the result is
+        the visible face defined by face landmarks.
+
+        Parameters
+        ----------
+        aligned : bool, optional
+            should this be applied on aligned image and mask or on raw image
+                and mask, by default True
+
+        Returns
+        -------
+        np.ndarray
+            image where only face is visible, without background
+        """
+        face = self.aligned_image
+        face_mask = self.aligned_mask
+        if not aligned:
+            face = self.raw_image.data
+            face_mask = self.mask
+        return np.multiply(face, face_mask).astype(np.uint8)
 
     def draw_landmarks(self) -> np.ndarray:
         """Draws small dots which represent face landmarks on the raw image
@@ -169,55 +205,3 @@ class Face:
             copy = cv.circle(copy, (x, y), radius=2,
                              color=(255, 0, 0), thickness=-1)
         return copy
-
-    def _add_landmark_to_mask(
-        self,
-        mask: np.ndarray,
-        landmarks: np.ndarray,
-    ) -> np.ndarray:
-        """Adds convex fill to the mask on spaces defined by landmarks. These
-        spaces can be specific face landmarks or whole face specified by
-        landmarks.
-
-        Parameters
-        ----------
-        mask : np.ndarray
-            mask on which to add convex fill
-        landmarks : np.ndarray
-            face landmarks
-
-        Returns
-        -------
-        np.ndarray
-            convex fill added on mask
-        """
-        landmarks = landmarks.astype(np.int32)
-
-        cv.fillConvexPoly(mask, cv.convexHull(landmarks), 1,)
-        dilate_h = mask.shape[0] // 64
-        dilate_w = mask.shape[1] // 64
-        mask = cv.dilate(
-            mask,
-            cv.getStructuringElement(cv.MORPH_ELLIPSE, (dilate_h, dilate_w)),
-            iterations=1,
-        )
-        return mask
-
-    @property
-    def mask(self) -> np.ndarray:
-        """Makes face mask based on the detected landmarks on the whole image.
-
-        Returns
-        -------
-        np.ndarray
-            face mask
-        """
-        h, w, _ = self.raw_image.shape
-        mask = np.zeros((h, w, 1), dtype=np.float32)
-
-        if self.landmarks is None:
-            raise NoLandmarksError()
-
-        mask = self._add_landmark_to_mask(mask, self.landmarks.dots)
-
-        return mask
