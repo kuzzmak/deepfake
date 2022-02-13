e2f539de2c58d780d7c6202fd915987e091195b3
kuzzmak <kuzminski.antonio@gmail.com>
2022-02-13 21:56:30 +0100

    Changed attribute names.

--- core/dataset/configuration.py
+++ core/dataset/configuration.py
@@ -10,20 +10,18 @@ class DatasetConfiguration:
 
     Parameters
     ----------
-    metadata_path_A : str
+    path_A : str
         path of the directory containing `Face` metadata of person A
-    metadata_path_B : str
+    path_B : str
         path of the directory containing `Face` metadata of person B
     batch_size : int
         batch size
-    input_shape : int
+    input_size : int
         size of the square image on the model input
-    output_shape : int
+    output_size : int
         size of the square image on the model output
     image_augmentaions : List[Callable]
         list of functions for image augmentation
-    size : int
-        size of the dataset, by default -1, load all
     data_transforms : Optional[transforms.Compose], optional
         transformations for the dataset, by default None
     shuffle : bool
@@ -31,13 +29,12 @@ class DatasetConfiguration:
     num_workers : int
         number of threads used for dataset loading, by default 2
     """
-    metadata_path_A: str
-    metadata_path_B: str
+    path_A: str
+    path_B: str
+    input_size: int
+    output_size: int
     batch_size: int
-    input_shape: int
-    output_shape: int
     image_augmentations: List[Callable] = field()
-    size: int = -1
     data_transforms: Optional[transforms.Compose] = None
     shuffle: bool = True
     num_workers: int = 2
