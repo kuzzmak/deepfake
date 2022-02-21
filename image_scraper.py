74d7fd8e5c858cbb80fb871968b804785fa61b0e
kuzzmak <kuzminski.antonio@gmail.com>
2022-02-21 01:24:31 +0100

    Refactored into class, now possible to download both main and suggested images.

--- image_scraper.py
+++ image_scraper.py
@@ -1,151 +1,215 @@
+import base64
+import logging
+import os
 from pathlib import Path
 import random
 import time
+from typing import List, Optional, Union
 
 import requests
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webelement import WebElement
+from tqdm import tqdm
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def check_if_result_b64(source):
-    possible_header = source.split(',')[0]
-    if possible_header.startswith('data') and ';base64' in possible_header:
-        image_type = possible_header.replace(
-            'data:image/',
-            '').replace(
-            ';base64',
-            '')
-        return image_type
-    return False
+class Scraper:
 
+    def __init__(self) -> None:
+        options = webdriver.ChromeOptions()
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) ' \
+            'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+            'Chrome/80.0.3987.132 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument('--disable-web-security')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument('--allow-cross-origin-auth-prompt')
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--ignore-ssl-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        options.add_argument('--log-level=3')
+        service = Service(
+            ChromeDriverManager(
+                log_level=logging.ERROR,
+                print_first_line=False,
+            ).install()
+        )
+        self._driver = webdriver.Chrome(
+            service=service,
+            options=options,
+        )
+        self._supported_mimes = set(['image/jpeg', 'image/png'])
+        self._mime_to_ext = {
+            'image/png': 'png',
+            'image/jpeg': 'jpg',
+        }
 
-supported_mimes = set(['image/jpeg', 'image/png'])
-mime_to_ext = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-}
-images_dir = Path('images')
+    @staticmethod
+    def _check_if_result_b64(source: str) -> Union[bool, str]:
+        possible_header = source.split(',')[0]
+        if possible_header.startswith('data') and ';base64' in possible_header:
+            image_type = possible_header \
+                .replace('data:image/', '') \
+                .replace(';base64', '')
+            return image_type
+        return False
 
-if __name__ == '__main__':
-    options = webdriver.ChromeOptions()
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                 'Chrome/80.0.3987.132 Safari/537.36'
-    options.add_argument(f'user-agent={user_agent}')
-    options.add_argument("--disable-web-security")
-    options.add_argument("--allow-running-insecure-content")
-    options.add_argument("--allow-cross-origin-auth-prompt")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
+    def close_driver(self) -> None:
+        self._driver.quit()
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(
-        service=service,
-        options=options,
-    )
-    driver.get('https://www.google.com/search?q=nicolas+cage&tbm=isch')
+    def _search_for(self, keywords: List[str]) -> None:
+        keywords = '+'.join(keywords)
+        link = f'https://www.google.com/search?q={keywords}&tbm=isch'
+        self._driver.get(link)
 
-    results = driver.find_elements(
-        By.CSS_SELECTOR,
-        '.isv-r.PNCib.MSM1fd.BUooTd',
-    )
-    last_results_num = len(results)
-
-    while True:
-        driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
-        # time to load images when scrolling
-        time.sleep(random.randint(2, 5))
-        results = driver.find_elements(
+    def _find_first_images(self) -> List[WebElement]:
+        return self._driver.find_elements(
             By.CSS_SELECTOR,
             '.isv-r.PNCib.MSM1fd.BUooTd',
         )
-        if len(results) == last_results_num:
-            break
-        last_results_num = len(results)
 
-    found = 0
-    for res in results:
-        res.click()
-        time.sleep(random.randint(2, 5))
-
-        elem = driver.find_element(
+    def _find_second_image(self) -> WebElement:
+        return self._driver.find_element(
             By.CSS_SELECTOR,
             '.tvh9oe.BIB1wf .eHAdSb>img',
         )
-        img_src = elem.get_attribute('src')
 
-        is_b64 = check_if_result_b64(img_src)
-        if is_b64:
-            print('je kod')
-        else:
+    def _exists_more(self) -> bool:
+        button = self._driver.find_element(
+            By.CSS_SELECTOR,
+            'div.YstHxe > input',
+        )
+        button_style = button.find_element(
+            By.XPATH,
+            '..'
+        ).get_attribute('style')
+        if button_style == '':
+            button.click()
+            return True
+        return False
+
+    def _scroll_to_page_end(self) -> None:
+        self._driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);"
+        )
+
+    def _sleep(self) -> None:
+        time.sleep(random.randint(2, 5))
+
+    def _get_suggested_button(self) -> WebElement:
+        return self._driver.find_element(
+            By.CSS_SELECTOR,
+            'div.O8VmIc.a3Wc3 > a'
+        )
+
+    def _find_all_images(self, first_page_only=False) -> List[WebElement]:
+        results = self._find_first_images()
+        if first_page_only:
+            return results
+        last_results_num = len(results)
+        while True:
+            print(f'Found {last_results_num} images.')
+            self._scroll_to_page_end()
+            self._sleep()
+            results = self._find_first_images()
+            if len(results) == last_results_num:
+                if self._exists_more():
+                    self._sleep()
+                else:
+                    break
+            last_results_num = len(results)
+        return results
+
+    def _download_images(
+        self,
+        images: List[WebElement],
+        save_directory: Path,
+    ) -> None:
+        found = len(os.listdir(save_directory))
+        for image in images:
+            image.click()
+            self._sleep()
             try:
-                resp = requests.get(img_src)
-            except requests.exceptions.RequestException:
+                elem = self._find_second_image()
+            except NoSuchElementException:
+                print('Error happened while getting second image, skipping.')
                 continue
-            contents = resp.content
-            mime_type = resp.headers['content-type']
-            if mime_type not in supported_mimes:
-                continue
+            img_src = elem.get_attribute('src')
+            print('Getting: ', img_src)
+            is_b64 = Scraper._check_if_result_b64(img_src)
+            if is_b64:
+                image_format = is_b64
+                contents = base64.b64decode(img_src.split(';base64')[1])
             else:
-                ext = mime_to_ext[mime_type]
-                filename = f'{str(found)}.{ext}'
-                with open(images_dir / filename, 'wb') as f:
-                    f.write(contents)
-                found += 1
-                print('downloaded: ', img_src)
+                try:
+                    resp = requests.get(img_src, timeout=10)
+                except requests.exceptions.RequestException:
+                    continue
+                contents = resp.content
+                mime_type = resp.headers.get('content-type', '')
+                if mime_type not in self._supported_mimes:
+                    continue
+                else:
+                    image_format = self._mime_to_ext[mime_type]
 
-    driver.quit()
+            found += 1
+            filename = f'{str(found)}.{image_format}'
+            with open(save_directory / filename, 'wb') as f:
+                f.write(contents)
+            print('downloaded: ', img_src)
 
-    # results = driver.find_elements(
-    #     By.CSS_SELECTOR,
-    #     '.isv-r.PNCib.MSM1fd.BUooTd',
-    # )
-    # found = 0
-    # # images that appear when searching on google images
-    # results = driver.find_elements(
-    #     By.CSS_SELECTOR,
-    #     '.isv-r.PNCib.MSM1fd.BUooTd',
-    # )
-    # for res in results:
-    #     res.click()
-    #     time.sleep(random.randint(5, 15) / 10)
-    #     # after click on some image, panel on right appears where this exact
-    #     # image is but in bigger resolution and button for suggested images
-    #     # which is then clicked
-    #     res = driver.find_element(
-    #         By.CSS_SELECTOR,
-    #         'div.O8VmIc.a3Wc3 > a'
-    #     )
-    #     res.click()
-    #     time.sleep(random.randint(5, 15) / 10)
-    #     # new page with suggested images
-    #     suggested_results = driver.find_elements(
-    #         By.CSS_SELECTOR,
-    #         '.isv-r.PNCib.MSM1fd.BUooTd',
-    #     )
-    #     for sugg_res in suggested_results:
-    #         sugg_res.click()
-    #         time.sleep(random.randint(5, 15) / 10)
-    #         # image element for a single image on this page
-    #         elem = driver.find_element(
-    #             By.CSS_SELECTOR,
-    #             '.tvh9oe.BIB1wf .eHAdSb>img',
-    #         )
-    #         img_src = elem.get_attribute('src')
-    #         resp = requests.get(img_src)
-    #         contents = resp.content
-    #         mime_type = resp.headers['content-type']
-    #         if mime_type not in supported_mimes:
-    #             continue
-    #         else:
-    #             ext = mime_to_ext[mime_type]
-    #             filename = f'{str(found)}.{ext}'
-    #             with open(images_dir / filename, 'wb') as f:
-    #                 f.write(contents)
-    #             found += 1
-    #             print('downloaded: ', img_src)
-    #     break
-    # driver.quit()
+    def run(
+        self,
+        keywords: List[str],
+        save_directory: Union[str, Path],
+        n_suggested: int,
+        from_n: Optional[int] = None,
+        to_n: Optional[int] = None,
+        skip_main_images: bool = False,
+    ) -> None:
+        if isinstance(save_directory, str):
+            save_directory = Path(save_directory)
+        if not save_directory.exists():
+            os.makedirs(save_directory)
+
+        self._search_for(keywords)
+        print('Finding main page images.')
+        images = self._find_all_images(skip_main_images)
+        print(f'Found {len(images)} images.')
+
+        print('Downloading main page images.')
+        self._download_images(images, save_directory)
+
+        # remember links of first n suggested images pages
+        first_n_links = []
+        for i in range(n_suggested):
+            images[i].click()
+            self._sleep()
+
+            sugg_button = self._get_suggested_button()
+            link = sugg_button.get_attribute('href')
+            first_n_links.append(link)
+
+        for i, link in enumerate(first_n_links):
+            self._driver.get(link)
+            self._sleep()
+
+            print(f'Finding suggested images for image {i+1}.')
+            sugg_images = self._find_all_images()
+            print(
+                f'Found {len(sugg_images)} suggested images ' +
+                f'for main image {i+1}.'
+            )
+            print('Downloading images.')
+            self._download_images(sugg_images, save_directory)
+            print('Images downloaded.')
+
+
+if __name__ == '__main__':
+    sc = Scraper()
+    sc.run(['donald', 'trump'], 'data/scraped/donald_trump', 10)
+    sc.close_driver()
