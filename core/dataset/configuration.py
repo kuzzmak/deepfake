0a1414fc7f6ad3bd90cb535191756a6e3717b6a5
kuzzmak <kuzminski.antoniovgmail.com>
2022-03-10 22:20:06 +0100

    Changed to normal class from dataclass and added conversion to Path.

--- core/dataset/configuration.py
+++ core/dataset/configuration.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-from typing import Callable, List, Optional
+from pathlib import Path
+from typing import Callable, List, Optional, Union
 
 from torchvision import transforms
 
@@ -10,9 +11,9 @@ class DatasetConfiguration:
 
     Parameters
     ----------
-    path_A : str
+    path_A : Path
         path of the directory containing `Face` metadata of person A
-    path_B : str
+    path_B : Path
         path of the directory containing `Face` metadata of person B
     batch_size : int
         batch size
@@ -29,12 +30,65 @@ class DatasetConfiguration:
     num_workers : int
         number of threads used for dataset loading, by default 2
     """
-    path_A: str
-    path_B: str
-    input_size: int
-    output_size: int
-    batch_size: int
-    image_augmentations: List[Callable] = field()
-    data_transforms: Optional[transforms.Compose] = None
-    shuffle: bool = True
-    num_workers: int = 2
+
+    def __init__(
+        self,
+        path_A: Union[str, Path],
+        path_B: Union[str, Path],
+        input_size: int,
+        output_size: int,
+        batch_size: int,
+        image_augmentations: List[Callable] = field(),
+        data_transforms: Optional[transforms.Compose] = None,
+        shuffle: bool = True,
+        num_workers: int = 2,
+    ) -> None:
+        if isinstance(path_A, str):
+            path_A = Path(path_A)
+        self._path_A = path_A
+        if isinstance(path_B, str):
+            path_B = Path(path_B)
+        self._path_B = path_B
+        self._input_size = input_size
+        self._output_size = output_size
+        self._batch_size = batch_size
+        self._image_augmentations = image_augmentations
+        self._data_transforms = data_transforms
+        self._shuffle = shuffle
+        self._num_workers = num_workers
+
+    @property
+    def path_A(self) -> Path:
+        return self._path_A
+
+    @property
+    def path_B(self) -> Path:
+        return self._path_B
+
+    @property
+    def input_size(self) -> int:
+        return self._input_size
+
+    @property
+    def output_size(self) -> int:
+        return self._output_size
+
+    @property
+    def batch_size(self) -> int:
+        return self._batch_size
+
+    @property
+    def image_augmentations(self) -> List[Callable]:
+        return self._image_augmentations
+
+    @property
+    def data_transforms(self) -> Optional[transforms.Compose]:
+        return self._data_transforms
+
+    @property
+    def shuffle(self) -> bool:
+        return self._shuffle
+
+    @property
+    def num_workers(self) -> int:
+        return self._num_workers
