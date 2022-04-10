89247fb860e6f77e37a7e33d046d9a47f368bcdd
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-11 00:33:23 +0200

    Added functionality for button detect deepfake page, refactored.

--- gui/pages/start_page.py
+++ gui/pages/start_page.py
@@ -1,20 +1,35 @@
+from typing import Dict, Optional
+
+import PyQt5.QtCore as qtc
+
+from enums import SIGNAL_OWNER
 from gui.pages.page import Page
 from gui.templates.start_page import Ui_start_page
-
-from names import START_PAGE_NAME, MAKE_DEEPFAKE_PAGE_NAME, START_PAGE_TITLE
+from names import (
+    DETECT_DEEPFAKE_PAGE_NAME,
+    START_PAGE_NAME,
+    MAKE_DEEPFAKE_PAGE_NAME,
+    START_PAGE_TITLE,
+)
 
 
 class StartPage(Page, Ui_start_page):
 
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, page_name=START_PAGE_NAME, *args, **kwargs)
+    def __init__(
+        self,
+        signals: Optional[Dict[SIGNAL_OWNER, qtc.pyqtSignal]] = None,
+    ):
+        super().__init__(signals, START_PAGE_NAME)
 
         self.setupUi(self)
-
         self.setWindowTitle(START_PAGE_TITLE)
+        self.make_deepfake_btn.clicked.connect(
+            lambda: self._goto(MAKE_DEEPFAKE_PAGE_NAME)
+        )
+        self.detect_deepfake_btn.clicked.connect(
+            lambda: self._goto(DETECT_DEEPFAKE_PAGE_NAME)
+        )
 
-        self.make_deepfake_btn.clicked.connect(self.goto_make_deepfake)
-
-    def goto_make_deepfake(self):
+    def _goto(self, page):
         self.show_toolbars_and_console(True)
-        self.goto(MAKE_DEEPFAKE_PAGE_NAME)
+        self.goto(page)
