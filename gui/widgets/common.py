357774fbb7e8d27b6852d64171a03b6e5cd3c71e
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-10 16:45:56 +0200

    Added common widgets, button, info button and group box.

--- gui/widgets/common.py
+++ gui/widgets/common.py
@@ -1,5 +1,7 @@
 import PyQt5.QtWidgets as qwt
 
+from enums import LAYOUT
+
 
 VerticalSpacer = qwt.QSpacerItem(
     1,
@@ -34,3 +36,33 @@ MinimalSizePolicy = qwt.QSizePolicy(
     qwt.QSizePolicy.Fixed,
     qwt.QSizePolicy.Fixed,
 )
+
+
+def Button(text: str = 'click', width: int = 150) -> qwt.QPushButton:
+    but = qwt.QPushButton(text=text)
+    but.setFixedWidth(width)
+    return but
+
+
+def GroupBox(
+    title: str = '',
+    layout: LAYOUT = LAYOUT.VERTICAL,
+) -> qwt.QGroupBox:
+    gb = qwt.QGroupBox()
+    gb.setLayout(
+        qwt.QVBoxLayout()
+        if layout == LAYOUT.VERTICAL
+        else qwt.QHBoxLayout()
+    )
+    gb.setTitle(title)
+    return gb
+
+
+def InfoButton(tooltip: str = '') -> Button:
+    button = Button('', 20)
+    button.setIcon(qwt.QApplication.style().standardIcon(
+        qwt.QStyle.SP_MessageBoxInformation
+    ))
+    button.setStyleSheet("border-radius: 10")
+    button.setToolTip(tooltip)
+    return button
