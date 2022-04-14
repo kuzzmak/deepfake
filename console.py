bcfd410a2a925ea39deff637128974baedf0cd81
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-14 19:27:19 +0200

    Updated console logging with source type.

--- console.py
+++ console.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import PyQt6.QtCore as qtc
 
 from config import APP_CONFIG
@@ -14,10 +16,10 @@ console_message_template = '<span style="font-size:{}pt; ' + \
 class Console(qtc.QObject):
 
     __instance = None
-    _int_print_sig = qtc.pyqtSignal(str, str, Level, str)
+    _int_print_sig = qtc.pyqtSignal(str, str, str, Level, str)
     print_sig = qtc.pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self) -> None:
         if Console.__instance is not None:
             raise Exception('This class shouldn\'t be manually initialized.')
         else:
@@ -26,18 +28,36 @@ class Console(qtc.QObject):
             self._int_print_sig.connect(self._print)
 
     @staticmethod
-    def get_instance():
+    def get_instance() -> Console:
         if Console.__instance is None:
             Console()
         return Console.__instance
 
     @staticmethod
-    def print(date: str, name: str, level: Level, msg: str):
-        Console.get_instance()._int_print_sig.emit(date, name, level, msg)
+    def print(
+            date: str,
+            name: str,
+            source_type: str,
+            level: Level,
+            msg: str) -> None:
+        Console.get_instance()._int_print_sig.emit(
+            date,
+            name,
+            source_type,
+            level,
+            msg,
+        )
 
-    @qtc.pyqtSlot(str, str, Level, str)
-    def _print(self, date: str, name: str, level: Level, msg: str):
-        prefix = self._get_prefix(date, name, level)
+    @qtc.pyqtSlot(str, str, str, Level, str)
+    def _print(
+        self,
+        date: str,
+        name: str,
+        source_type: str,
+        level: Level,
+        msg: str,
+    ) -> None:
+        prefix = self._get_prefix(date, name, source_type, level)
         msg = console_message_template.format(
             APP_CONFIG.app.gui.widgets.console.text_size,
             COLOR.WHITE.value,
@@ -46,10 +66,15 @@ class Console(qtc.QObject):
         self.print_sig.emit(prefix + msg)
 
     @staticmethod
-    def _get_prefix(date: str, name: str, level: Level):
+    def _get_prefix(
+        date: str,
+        name: str,
+        source_type: str,
+        level: Level,
+    ) -> str:
         prefix = console_message_template.format(
             APP_CONFIG.app.gui.widgets.console.text_size,
             LevelColor[level.value].value.value,
-            f'[{date}] - {name} - {level.value} - '
+            f'[{date}] - [{source_type}] - {name} - {level.value} - '
         )
         return prefix
