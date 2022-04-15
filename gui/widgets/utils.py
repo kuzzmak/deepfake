f78d137e37b773d87ed4f3c8a725557b07beb99a
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-15 02:04:53 +0200

    Updated to PyQt6.

--- gui/widgets/utils.py
+++ gui/widgets/utils.py
@@ -4,7 +4,7 @@ import PyQt6.QtWidgets as qwt
 
 def set_color_on_widget(
     widget: qwt.QWidget,
-    color: qtc.Qt.GlobalColor = qtc.Qt.red,
+    color: qtc.Qt.GlobalColor = qtc.Qt.GlobalColor.red,
 ):
     """Sets some color on the widget. Can be used for styling or debugging
     purposes where true widget size can be seen only when color is applied.
