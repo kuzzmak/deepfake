a0f8d52e41a9ea6fabc8d8a9a7418caabae15034
kuzzmak <kuzminski.antonio@gmail.com>
2022-04-16 14:42:18 +0200

    Refactored and formatted.

--- core/worker/landmark_extraction_worker.py
+++ core/worker/landmark_extraction_worker.py
@@ -8,22 +8,13 @@ import PyQt6.QtCore as qtc
 
 from core.df_detection.mri_gan.data_utils.face_detection import \
     extract_landmarks_from_video
-from core.df_detection.mri_gan.data_utils.utils import \
-    get_dfdc_training_video_filepaths
-from core.df_detection.mri_gan.utils import ConfigParser
+from core.worker.mri_gan_worker import MRIGANWorker
 from core.worker.worker_with_pool import WorkerWithPool
-from enums import (
-    BODY_KEY,
-    JOB_TYPE,
-    MESSAGE_STATUS,
-    MESSAGE_TYPE,
-    SIGNAL_OWNER,
-    WIDGET,
-)
-from message.message import Body, Message, Messages
+from enums import DATA_TYPE, JOB_TYPE, SIGNAL_OWNER, WIDGET
+from message.message import Messages
 
 
-class LandmarkExtractionWorker(WorkerWithPool):
+class LandmarkExtractionWorker(MRIGANWorker, WorkerWithPool):
     """Worker used to extact face landmarks from the images with faces.
 
     Args:
@@ -37,38 +28,37 @@ class LandmarkExtractionWorker(WorkerWithPool):
 
     def __init__(
         self,
+        data_type: DATA_TYPE,
         num_instances: int = 2,
         message_worker_sig: Optional[qtc.pyqtSignal] = None,
     ) -> None:
-        super().__init__(num_instances, message_worker_sig)
+        MRIGANWorker.__init__(self, data_type)
+        WorkerWithPool.__init__(self, num_instances, message_worker_sig)
 
     def run_job(self) -> None:
         self.logger.info(
-            'Started landmark extraction process for DFDC dataset.'
+            'Started landmark extraction process on DFDC ' +
+            f'{self._data_type.value} dataset.'
         )
-        data_path_root = ConfigParser.getInstance().get_dfdc_train_data_path()
-        self.logger.info(
-            f'Starting landmark extraction in directory: {data_path_root}.'
-        )
-        file_paths = get_dfdc_training_video_filepaths(data_path_root)
-        self.logger.info(f'Found {len(file_paths)} videos in directory.')
-        out_dir = ConfigParser.getInstance().get_dfdc_landmarks_train_path()
+        data_paths = self._get_data_paths()
+        self.logger.info(f'Found {len(data_paths)} videos in directory.')
+        out_dir = self._get_dfdc_landmarks_data_path()
         self.logger.info(f'Landmarks metadata will be saved in {out_dir}.')
         os.makedirs(out_dir, exist_ok=True)
 
         self.logger.debug(
-            f'Launching landmark extraction with ' +
+            'Launching landmark extraction with ' +
             f'{self._num_instances} processes.'
         )
 
         with multiprocessing.Pool(self._num_instances) as pool:
             jobs: List[AsyncResult] = []
             results = []
-            for fp in file_paths:
+            for dp in data_paths:
                 jobs.append(
                     pool.apply_async(
                         extract_landmarks_from_video,
-                        (fp, out_dir,),
+                        (dp, out_dir,),
                     )
                 )
             if self._message_worker_sig is not None:
@@ -93,22 +83,11 @@ class LandmarkExtractionWorker(WorkerWithPool):
 
                 results.append(job.get())
 
-                if self._message_worker_sig is not None:
-                    job_prog_msg = Message(
-                        MESSAGE_TYPE.ANSWER,
-                        MESSAGE_STATUS.OK,
-                        SIGNAL_OWNER.LANDMARK_EXTRACTION_WORKER,
-                        SIGNAL_OWNER.JOB_PROGRESS,
-                        Body(
-                            JOB_TYPE.LANDMARK_EXTRACTION,
-                            {
-                                BODY_KEY.PART: idx,
-                                BODY_KEY.TOTAL: len(jobs),
-                                BODY_KEY.JOB_NAME: 'landmark extraction'
-                            },
-                            idx == len(jobs) - 1,
-                        )
-                    )
-                    self.send_message(job_prog_msg)
+                self.report_progress(
+                    SIGNAL_OWNER.LANDMARK_EXTRACTION_WORKER,
+                    JOB_TYPE.LANDMARK_EXTRACTION,
+                    idx,
+                    len(jobs),
+                )
 
         self.logger.info('Landmark extraction finished.')
