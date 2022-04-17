3f24024093bf3aff0b0550b3dceda5075b361707
kuzzmak <kuzminski.antonio@gmail.com>
2022-04-17 21:11:11 +0200

    Step widget for mri gan page.

--- gui/pages/detect_deepfake_page/mri_gan/common.py
+++ gui/pages/detect_deepfake_page/mri_gan/common.py
@@ -1,7 +1,7 @@
 import PyQt6.QtWidgets as qwt
-from enums import DATA_TYPE
 
-from gui.widgets.common import HWidget
+from enums import DATA_TYPE, LAYOUT
+from gui.widgets.common import Button, GroupBox, HWidget, PlayIcon, VWidget
 
 
 class NumOfInstancesRow(qwt.QWidget):
@@ -67,3 +67,69 @@ class DataTypeRadioButtons(qwt.QWidget):
         for but in self.data_btn_bg.buttons():
             if but.isChecked():
                 return DATA_TYPE[but.text().upper()]
+
+
+class Step(qwt.QWidget):
+
+    def __init__(self, gb_name: str, start_btn_name: str) -> None:
+        """Widget containing entry for number of instances which controlls how
+        many processes or threads will be spawned when some job is starting,
+        selector for what kind of data will be used: train, test, valid or
+        everything, button for starting the job and button for configuring
+        paths for the job. This four components are base, possible to add
+        additional widgets and functionality from the outside.
+
+        Parameters
+        ----------
+        gb_name : str
+            name of the step
+        start_btn_name : str
+            text on the button which starts job
+        """
+        super().__init__()
+
+        self._init_ui(gb_name, start_btn_name)
+
+    def _init_ui(self, gb_name: str, start_btn_name) -> None:
+        layout = qwt.QVBoxLayout()
+        self.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        gb = GroupBox(gb_name, LAYOUT.HORIZONTAL)
+        layout.addWidget(gb)
+        gb.setMaximumWidth(400)
+
+        self.left_part = VWidget()
+        gb.layout().addWidget(self.left_part)
+
+        self._num_of_instances = NumOfInstancesRow()
+        self.left_part.layout().addWidget(
+            self._num_of_instances
+        )
+
+        self.radio_btns = DataTypeRadioButtons()
+        self.left_part.layout().addWidget(
+            self.radio_btns
+        )
+
+        right_part = VWidget()
+        gb.layout().addWidget(right_part)
+
+        self.start_btn = Button(start_btn_name)
+        right_part.layout().addWidget(
+            self.start_btn
+        )
+        self.start_btn.setIcon(PlayIcon())
+
+        self.configure_paths_btn = Button('configure paths')
+        right_part.layout().addWidget(
+            self.configure_paths_btn
+        )
+
+    @property
+    def selected_data_type(self) -> DATA_TYPE:
+        return self.radio_btns.selected_data_type
+
+    @property
+    def num_of_instances(self) -> str:
+        return self._num_of_instances.num_of_instances_value
