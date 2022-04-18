fb64e32bee0a2f8ec86d624dedf2131bfe776a80
kuzzmak <kuzminski.antonio@gmail.com>
2022-04-19 00:59:31 +0200

    Added init file for shorter import paths.

---
+++ core/worker/__init__.py
@@ -0,0 +1,9 @@
+from .crop_faces_worker import CropFacesWorker
+from .generate_mri_dataset_worker import GenerateMRIDatasetWorker
+from .landmark_extraction_worker import LandmarkExtractionWorker
+
+__all__ = [
+    'CropFacesWorker',
+    'LandmarkExtractionWorker',
+    'GenerateMRIDatasetWorker',
+]
