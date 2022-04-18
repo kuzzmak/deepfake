da27df9dbee56c37d68ce6d9e5b89a2d9198d1b4
kuzzmak <kuzminski.antonio@gmail.com>
2022-04-19 01:23:52 +0200

    Exported new MRIGANWorker.

--- core/worker/__init__.py
+++ core/worker/__init__.py
@@ -1,9 +1,11 @@
 from .crop_faces_worker import CropFacesWorker
 from .generate_mri_dataset_worker import GenerateMRIDatasetWorker
 from .landmark_extraction_worker import LandmarkExtractionWorker
+from .mri_gan_worker import MRIGANWorker
 
 __all__ = [
     'CropFacesWorker',
     'LandmarkExtractionWorker',
     'GenerateMRIDatasetWorker',
+    'MRIGANWorker',
 ]
