5ab801dd8504f37be25ce1ac3ca36b7e3131e0de
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-19 14:39:32 +0200

    Changed order of imports to fix import error.

--- core/worker/__init__.py
+++ core/worker/__init__.py
@@ -1,7 +1,7 @@
+from .mri_gan_worker import MRIGANWorker
 from .crop_faces_worker import CropFacesWorker
 from .generate_mri_dataset_worker import GenerateMRIDatasetWorker
 from .landmark_extraction_worker import LandmarkExtractionWorker
-from .mri_gan_worker import MRIGANWorker
 
 __all__ = [
     'CropFacesWorker',
