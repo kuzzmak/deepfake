04096c44fa01b6bf5a564ef988158e720bc29535
kuzzmak <kuzminski.antoniovgmail.com>
2022-04-20 17:20:26 +0200

    Exported train mri gan worker.

--- core/worker/__init__.py
+++ core/worker/__init__.py
@@ -4,6 +4,7 @@ from .worker_with_pool import WorkerWithPool
 from .crop_faces_worker import CropFacesWorker
 from .generate_mri_dataset_worker import GenerateMRIDatasetWorker
 from .landmark_extraction_worker import LandmarkExtractionWorker
+from .train_mri_gan_worker import TrainMRIGANWorker
 
 __all__ = [
     'CropFacesWorker',
@@ -12,4 +13,5 @@ __all__ = [
     'MRIGANWorker',
     'Worker',
     'WorkerWithPool',
+    'TrainMRIGANWorker',
 ]
