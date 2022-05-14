d7dd5da2af6bf2562bb871964f83028ab92d85f5
kuzzmak <kuzminski.antoniovgmail.com>
2022-05-14 22:53:45 +0200

    Added layout with no margins function and widget for available devices.

--- gui/widgets/common.py
+++ gui/widgets/common.py
@@ -1,7 +1,9 @@
+from typing import Union
 import PyQt6.QtGui as qtg
 import PyQt6.QtWidgets as qwt
 
-from enums import LAYOUT
+from config import APP_CONFIG
+from enums import DEVICE, LAYOUT
 
 
 def VerticalSpacer() -> qwt.QSpacerItem:
@@ -83,3 +85,62 @@ def StopIcon() -> qtg.QIcon:
     return qwt.QApplication.style().standardIcon(
         qwt.QStyle.StandardPixmap.SP_MediaStop
     )
+
+
+def NoMarginLayout(
+    layout: LAYOUT = LAYOUT.VERTICAL,
+) -> Union[qwt.QHBoxLayout, qwt.QVBoxLayout]:
+    """Used for constructing widget layout based on the type of the layout
+    `layout` which has no margins.
+
+    Parameters
+    ----------
+    layout : LAYOUT, optional
+        layout type, by default LAYOUT.VERTICAL
+
+    Returns
+    -------
+    Union[qwt.QHBoxLayout, qwt.QVBoxLayout]
+        layout with no margins based on thy type of the argument
+    """
+    if layout == LAYOUT.VERTICAL:
+        _lay = qwt.QVBoxLayout()
+    else:
+        _lay = qwt.QHBoxLayout()
+    _lay.setContentsMargins(0, 0, 0, 0)
+    return _lay
+
+
+class DeviceWidget(qwt.QWidget):
+    """Widget containing available devices on which some process could run.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._init_ui()
+
+    def _init_ui(self) -> None:
+        layout = qwt.QVBoxLayout()
+        self.setLayout(layout)
+        layout.setContentsMargins(0, 0, 0, 0)
+        gb = GroupBox('Device', LAYOUT.HORIZONTAL)
+        self._device_bg = qwt.QButtonGroup(gb)
+        layout.addWidget(gb)
+        for device in APP_CONFIG.app.core.devices:
+            btn = qwt.QRadioButton(device.value, gb)
+            btn.setChecked(True)
+            self._device_bg.addButton(btn)
+            gb.layout().addWidget(btn)
+
+    @property
+    def device(self) -> DEVICE:
+        """Currently selected device.
+
+        Returns:
+            DEVICE: cpu or cuda
+        """
+        for but in self._device_bg.buttons():
+            if but.isChecked():
+                return DEVICE[but.text().upper()]
+        return DEVICE.CPU
