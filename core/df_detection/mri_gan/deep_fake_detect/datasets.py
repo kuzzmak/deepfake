2be692080187d6b2929bebeedde9d2648cec49ba
kuzzmak <kuzminski.antoniovgmail.com>
2022-05-17 01:16:05 +0200

    Updated functionality to new way of loading data.

--- core/df_detection/mri_gan/deep_fake_detect/datasets.py
+++ core/df_detection/mri_gan/deep_fake_detect/datasets.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import random
 
 import numpy as np
@@ -6,80 +7,107 @@ import pandas as pd
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
+from torchvision.transforms import transforms
+from configs.mri_gan_config import MRIGANConfig
 
 from core.df_detection.mri_gan.utils import ConfigParser
+from enums import MODE, MRI_GAN_DATASET
 
 
 class DFDCDatasetSimple(Dataset):
+
     def __init__(
         self,
-        mode=None,
-        transform=None,
-        data_size=1,
-        dataset=None,
-        label_smoothing=0,
+        mode: MODE,
+        transform: transforms.Compose,
+        data_size: int,
+        dataset: MRI_GAN_DATASET,
+        label_smoothing: float = 0.,
     ):
         super().__init__()
         self.mode = mode
         # use only in training, so update to param passed in train mode
         self.label_smoothing = 0
-        if mode == 'train':
-            if dataset == 'plain':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+        if mode == MODE.TRAIN:
+            if dataset == MRI_GAN_DATASET.PLAIN:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_train_frame_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_crops_train_path()
-            elif dataset == 'mri':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+                )
+            elif dataset == MRI_GAN_DATASET.MRI:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_train_mriframe_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_train_mrip2p_png_data_path()
+                )
             else:
                 raise Exception('Bad dataset in DFDCDatasetSimple')
 
             self.label_smoothing = label_smoothing
-        elif mode == 'valid':
-            if dataset == 'plain':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+        elif mode == MODE.VALID:
+            if dataset == MRI_GAN_DATASET.PLAIN:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_valid_frame_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_crops_valid_path()
-            elif dataset == 'mri':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+                )
+            elif dataset == MRI_GAN_DATASET.MRI:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_valid_mriframe_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_valid_mrip2p_png_data_path()
+                )
             else:
                 raise Exception('Bad dataset in DFDCDatasetSimple')
 
-        elif mode == 'test':
-            if dataset == 'plain':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+        elif mode == MODE.TEST:
+            if dataset == MRI_GAN_DATASET.PLAIN:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_test_frame_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_dfdc_crops_test_path()
-            elif dataset == 'mri':
-                self.labels_csv = ConfigParser \
-                    .getInstance() \
+                )
+            elif dataset == MRI_GAN_DATASET.MRI:
+                self.labels_csv = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_test_mriframe_label_csv_path()
-                self.crops_dir = ConfigParser \
-                    .getInstance() \
+                )
+                self.crops_dir = Path(
+                    MRIGANConfig
+                    .get_instance()
                     .get_test_mrip2p_png_data_path()
+                )
             else:
                 raise Exception('Bad dataset in DFDCDatasetSimple')
         else:
             raise Exception('Bad mode in DFDCDatasetSimple')
-
         self.data_df = pd.read_csv(self.labels_csv)
         if data_size < 1:
             total_data_len = int(len(self.data_df) * data_size)
@@ -95,24 +123,18 @@ class DFDCDatasetSimple(Dataset):
         while True:
             try:
                 item = self.data_dict[index].copy()
-                frame = Image.open(
-                    os.path.join(
-                        self.crops_dir,
-                        str(item['video_id']),
-                        item['frame'],
-                    )
-                )
+                part, video_id, frame_name, label = item.values()
+                frame_path = self.crops_dir / part / video_id / frame_name
+                frame = Image.open(frame_path)
                 if self.transform is not None:
                     frame = self.transform(frame)
                 item['frame_tensor'] = frame
                 if self.label_smoothing != 0:
                     label = np.clip(
-                        item['label'],
+                        label,
                         self.label_smoothing,
                         1 - self.label_smoothing,
                     )
-                else:
-                    label = item['label']
                 item['label'] = torch.tensor(label)
                 return item
             except Exception as e:
