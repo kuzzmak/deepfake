1b02007a97929ae692b41db6f3a7802b36dd078a
kuzzmak <kuzminski.antonio@gmail.com>
2022-05-24 00:20:08 +0200

    Updated drag and drop widget to accept video files.

--- gui/pages/detect_deepfake_page/mri_gan/common.py
+++ gui/pages/detect_deepfake_page/mri_gan/common.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any, List, Optional, Union
 
+import cv2 as cv
 import PyQt6.QtGui as qtg
 import PyQt6.QtCore as qtc
 import PyQt6.QtWidgets as qwt
@@ -248,8 +249,9 @@ class Parameter(qwt.QWidget):
     def _init_ui(self) -> None:
         layout = NoMarginLayout(LAYOUT.HORIZONTAL)
         self.setLayout(layout)
-        layout.addWidget(qwt.QLabel(text=self._name))
-        layout.addItem(HorizontalSpacer())
+        if self._name:
+            layout.addWidget(qwt.QLabel(text=self._name))
+            layout.addItem(HorizontalSpacer())
 
         if self._wt == WIDGET_TYPE.INPUT:
             self._input = qwt.QLineEdit()
@@ -384,21 +386,21 @@ class DragAndDrop(qwt.QLabel):
     """Widget representing a drag and drop zone for images.
     """
 
-    image_path_sig = qtc.pyqtSignal(str)
+    # image_path_sig = qtc.pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
 
         self._init_ui()
 
-        self._image_path = None
+        self._file_path = None
 
-        self.image_path_sig.connect(self._set_image)
+        # self.image_path_sig.connect(self._set_image)
 
     @property
-    def image_path(self) -> Union[Path, None]:
-        if self._image_path is not None:
-            return Path(self._image_path)
+    def file_path(self) -> Union[Path, None]:
+        if self._file_path is not None:
+            return Path(self._file_path)
         return None
 
     def _init_ui(self) -> None:
@@ -415,25 +417,52 @@ class DragAndDrop(qwt.QLabel):
         super().setPixmap(image)
 
     def dragEnterEvent(self, event: qtc.QEvent):
-        if event.mimeData().hasImage:
+        if event.mimeData().hasUrls():
             event.accept()
         else:
             event.ignore()
 
     def dragMoveEvent(self, event: qtc.QEvent):
-        if event.mimeData().hasImage:
+        if event.mimeData().hasUrls():
             event.accept()
         else:
             event.ignore()
 
     def dropEvent(self, event: qtc.QEvent):
-        if event.mimeData().hasImage:
-            event.setDropAction(qtc.Qt.DropAction.CopyAction)
-            file_path = event.mimeData().urls()[0].toLocalFile()
-            self._set_image(file_path)
+        if event.mimeData().hasUrls():
+            for url in event.mimeData().urls():
+                self._open_video(url)
+
+            # event.setDropAction(qtc.Qt.DropAction.CopyAction)
+            # file_path = event.mimeData().urls()[0].toLocalFile()
+            # self._set_image(file_path)
             event.accept()
         else:
             event.ignore()
+
+    def _open_video(self, filename: qtc.QUrl) -> None:
+        path = filename.toLocalFile()
+        self._file_path = path
+        cap = cv.VideoCapture(path)
+        cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+        # width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+        # height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+        ret, frame = cap.read()
+        if not ret:
+            return
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        img = qtg.QImage(
+            frame, frame.shape[1],
+            frame.shape[0],
+            qtg.QImage.Format.Format_RGB888,
+        )
+        pix = qtg.QPixmap.fromImage(img)
+        pix = pix.scaled(
+            self.size(),
+            qtc.Qt.AspectRatioMode.KeepAspectRatio,
+            qtc.Qt.TransformationMode.SmoothTransformation,
+        )
+        self.setPixmap(pix)
 
     @qtc.pyqtSlot(str)
     def _set_image(self, file_path: str):
