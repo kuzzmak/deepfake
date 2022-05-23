a4c837dac35d47cfb3926b4805eae243af456a77
kuzzmak <kuzminski.antonio@gmail.com>
2022-05-24 00:19:35 +0200

    Implemented deepfake detection on video.

--- core/worker/infer_df_detector_worker.py
+++ core/worker/infer_df_detector_worker.py
@@ -1,17 +1,28 @@
 import logging
 from pathlib import Path
+import tempfile
 from typing import Optional, Union
 
-from PIL import Image
+import numpy as np
 import PyQt6.QtCore as qtc
 import torch
+from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
 from configs.mri_gan_config import MRIGANConfig
-from core.df_detection.mri_gan.data_utils.face_detection import get_face_detector_model
+from core.df_detection.mri_gan.data_utils.datasets import SimpleImageFolder
+from core.df_detection.mri_gan.data_utils.face_detection import (
+    crop_faces_from_video,
+    extract_landmarks_from_video,
+)
 from core.df_detection.mri_gan.deep_fake_detect.DeepFakeDetectModel import \
     DeepFakeDetectModel
-from core.df_detection.mri_gan.deep_fake_detect.utils import ENCODER_PARAMS, get_predictions, get_probability
+from core.df_detection.mri_gan.deep_fake_detect.utils import (
+    ENCODER_PARAMS,
+    get_predictions,
+    get_probability,
+    pred_strategy,
+)
 from core.worker import ContinuousWorker
 from enums import DEVICE, JOB_DATA_KEY
 from utils import prepare_path
@@ -67,27 +78,6 @@ class InferDFDetectorWorker(ContinuousWorker):
         self._model.eval()
         logger.debug('Model loaded.')
 
-    def _detect_face(self, image: Image.Image) -> Union[Image.Image, None]:
-        detector = get_face_detector_model()
-        boxes, _ = detector.detect(image)
-        if boxes is None:
-            return None
-        boxes = [b.tolist() if b is not None else None for b in boxes]
-        xmin, ymin, xmax, ymax = [int(b) for b in boxes[0]]
-        w = xmax - xmin
-        h = ymax - ymin
-        buf = 0.10
-        p_h = int(h * buf)
-        p_w = int(w * buf)
-        return image.crop(
-            (
-                max(ymin - p_h, 0),
-                max(xmin - p_w, 0),
-                ymax + p_h,
-                xmax + p_w,
-            )
-        )
-
     def run_job(self) -> None:
         if self._model is None:
             self._load_model()
@@ -96,41 +86,104 @@ class InferDFDetectorWorker(ContinuousWorker):
 
         self.running.emit()
 
-        path = self._current_job.data.get(JOB_DATA_KEY.IMAGE_PATH, None)
+        path = self._current_job.data.get(JOB_DATA_KEY.FILE_PATH, None)
         if path is None:
             logger.error(
-                f'Key {JOB_DATA_KEY.IMAGE_PATH.value} must be present.'
+                f'Key {JOB_DATA_KEY.FILE_PATH.value} must be present.'
             )
             return
         path = prepare_path(path)
         if path is None:
-            logger.error(f'Unable to parse image path {str(path)}.')
+            logger.error(f'Unable to parse file path {str(path)}.')
             return
 
-        image = Image.open(path)
+        root_dir = tempfile.TemporaryDirectory()
+        root_path = Path(root_dir.name)
+        plain_faces_data_dir = root_path / 'plain_frames'
+        plain_faces_data_dir.mkdir(exist_ok=True)
 
-        face = self._detect_face(image)
+        logger.debug(f'Extracting landmarks from video {str(path)}.')
+        extract_landmarks_from_video(
+            path,
+            root_path,
+            overwrite=True,
+            inference=True,
+        )
+        logger.debug('Landmark extraction done.')
 
-        if face is None:
-            logger.warning(
-                f'Was not able to detect face on image {str(path)}.'
-            )
-            return
+        logger.debug(f'Cropping faces from video {str(path)}.')
+        crop_faces_from_video(
+            path,
+            root_path,
+            plain_faces_data_dir,
+            overwrite=True,
+            inference=True,
+        )
+        logger.debug('Face cropping finished.')
 
         encoder_name = MRIGANConfig \
             .get_instance() \
             .get_default_cnn_encoder_name()
         image_size = ENCODER_PARAMS[encoder_name]['imsize']
-        face = _image_transforms(image_size)(face)
-        face = face.unsqueeze(0)
 
-        print('size', face.size())
-
+        test_transform = _image_transforms(image_size)
+        data_path = plain_faces_data_dir / path.stem
+        test_dataset = SimpleImageFolder(data_path, test_transform)
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=32,
+            num_workers=8,
+            pin_memory=True,
+        )
+        probabilities = []
+        prob_threshold_fake = 0.5
+        fake_fraction = 0.5
         with torch.no_grad():
-            output = self._model(face)
+            for samples in test_loader:
+                frames = samples.to(self._device.value)
+                output = self._model(frames)
+                predicted = get_predictions(output).to('cpu').detach().numpy()
+                class_probability = get_probability(output).to('cpu').detach().numpy()
+                if len(predicted) > 1:
+                    probabilities.extend(class_probability.squeeze())
+                else:
+                    probabilities.append(class_probability.squeeze())
 
-        output = output.squeeze(0)
-        preds = get_predictions(output).to('cpu').detach().numpy()
-        class_probability = get_probability(output).to('cpu').detach().numpy()
+            total_number_frames = len(probabilities)
+            probabilities = np.array(probabilities)
 
-        print(preds, class_probability)
+            fake_frames_high_prob = probabilities[
+                probabilities >= prob_threshold_fake
+            ]
+            number_fake_frames = len(fake_frames_high_prob)
+            if number_fake_frames == 0:
+                fake_prob = 0
+            else:
+                fake_prob = round(
+                    sum(fake_frames_high_prob) /
+                    number_fake_frames,
+                    4,
+                )
+
+            real_frames_high_prob = probabilities[
+                probabilities < prob_threshold_fake
+            ]
+            number_real_frames = len(real_frames_high_prob)
+            if number_real_frames == 0:
+                real_prob = 0
+            else:
+                real_prob = 1 - round(
+                    sum(real_frames_high_prob) / number_real_frames,
+                    4,
+                )
+
+            pred = pred_strategy(
+                number_fake_frames,
+                number_real_frames,
+                total_number_frames,
+                fake_fraction=fake_fraction,
+            )
+
+            print(fake_prob, real_prob, pred)
+
+        root_dir.cleanup()
