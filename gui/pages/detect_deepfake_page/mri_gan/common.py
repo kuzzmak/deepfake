a7c0b5523291e527dc3cda502f8e566c6c2a5280
kuzzmak <kuzminski.antonio@gmail.com>
2022-05-27 22:17:20 +0200

    Bug fix, passing default_value which was not a list, inserting wrong widget to the layout.

--- gui/pages/detect_deepfake_page/mri_gan/common.py
+++ gui/pages/detect_deepfake_page/mri_gan/common.py
@@ -230,8 +230,8 @@ class PredictMRIStep(Step):
         self.__init_ui()
 
     def __init_ui(self) -> None:
-        self.batch_size_par = Parameter('batch size', str(8))
-        self.left_part.layout().insertWidget(0, self.batch_size)
+        self.batch_size_par = Parameter('batch size', [str(8)])
+        self.left_part.layout().insertWidget(0, self.batch_size_par)
         self.device_row = DeviceRow()
         self.left_part.layout().insertWidget(0, self.device_row)
 
