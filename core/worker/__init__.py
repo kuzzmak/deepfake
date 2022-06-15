ebc7d5bcc337c42a2c830d31ee616e771d7655be
kuzzmak <kuzminski.antonio@gmail.com>
2022-06-15 13:49:01 +0200

    Exporting new infer df model worker.

--- core/worker/__init__.py
+++ core/worker/__init__.py
@@ -1,6 +1,7 @@
 from .mri_gan_worker import MRIGANWorker
 from .worker import Worker
 from .worker_with_pool import WorkerWithPool
+from .face_extraction_worker import FaceExtractionWorker
 from .crop_faces_worker import CropFacesWorker
 from .generate_mri_dataset_worker import GenerateMRIDatasetWorker
 from .landmark_extraction_worker import LandmarkExtractionWorker
@@ -11,8 +12,10 @@ from .predict_mri_worker import PredictMRIWorker
 from .generate_frame_labels_csv_worker import GenerateFrameLabelsCSVWorker
 from .continuous_worker import ContinuousWorker
 from .infer_df_detector_worker import InferDFDetectorWorker
+from .infer_df_model_worker import InferDFModelWorker
 
 __all__ = [
+    'FaceExtractionWorker',
     'CropFacesWorker',
     'GenerateMRIDatasetWorker',
     'LandmarkExtractionWorker',
@@ -26,4 +29,5 @@ __all__ = [
     'GenerateFrameLabelsCSVWorker',
     'InferDFDetectorWorker',
     'ContinuousWorker',
+    'InferDFModelWorker',
 ]
