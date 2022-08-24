4d0f07b2eb7b51968758d164934f9ab696a90c10
kuzzmak <kuzminski.antonio@gmail.com>
2022-08-24 22:12:04 +0200

    Updated with new logging config.

--- core/worker/fs_trainer_worker.py
+++ core/worker/fs_trainer_worker.py
@@ -2,11 +2,17 @@ from pathlib import Path
 from typing import Optional, Union
 
 import PyQt6.QtCore as qtc
+import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as T
 
 from core.dataset.dataset import FSDataset
+from core.model.configuration import ModelConfig
+from core.trainer.fs_trainer import FSTrainer
+from core.model.fs import FS
+from core.trainer.trainer import StepTrainerConfiguration
 from core.worker import Worker
+from df_logging.model_logging import LoggingConfig
 from variables import IMAGENET_MEAN, IMAGENET_STD
 
 
@@ -38,6 +44,7 @@ class FSTrainerWorker(Worker):
         self._lambda_feat = lambda_feat
         self._lambda_rec = lambda_rec
         self._use_cudnn_bench = use_cudnn_benchmark
+        self._device = torch.device('cuda')
 
     def run_job(self) -> None:
         transforms = T.Compose([
@@ -72,6 +79,35 @@ class FSTrainerWorker(Worker):
             'sample_freq': 10,
             'model_freq': 500,
         }
-
+        model_conf = ModelConfig(
+            model=FS,
+            options=model_options,
+        )
+        logging_config = LoggingConfig(
+            model_name='FS',
+            log_frequency=100,
+            sample_frequency=500,
+            checkpoint_frequency=500,
+            run_name=None,
+        )
+        logs_dir = Path('logs')
+        logs_dir.mkdir(exist_ok=True)
+        run = None
+        conf = StepTrainerConfiguration(
+            train_data_loader,
+            batch_size=self._batch_size,
+            steps=self._steps,
+            model_config=model_conf,
+            project='fs_trainer',
+            run_name=run,
+            resume_run=True,
+            device=self._device,
+            logging_dir=logs_dir.absolute(),
+            log_freq=50,
+            use_cudnn_benchmark=self._use_cudnn_bench,
+            wandb=True,
+        )
+        trainer = FSTrainer(conf)
+        trainer.start()
         self.running.emit()
         self._logger.debug('Started custom job.')
