871e6be10037090d7b80f9804a468dfbb7f5e2b7
kuzzmak <kuzminski.antonio@gmail.com>
2022-10-24 22:43:29 +0200

    Changed how optim gets its default args.

--- core/optimizer/configuration.py
+++ core/optimizer/configuration.py
@@ -1,22 +1,35 @@
 from dataclasses import dataclass, field
+import inspect
+from typing import Any, Dict, Union
 
+from torch.optim import Adam
 from enums import OPTIMIZER
 
 
 @dataclass
 class OptimizerConfiguration:
-    optimizer: OPTIMIZER
-    optimizer_args: dict = field(default_factory=dict)
+    optimizer_class: OPTIMIZER
+    optimizer_module: str
+    args: dict = field(default_factory=dict)
+
+    def values(self) -> Dict[str, Union[str, Dict[str, Any]]]:
+        return {
+            'optimizer_module': self.optimizer_module,
+            'optimizer_class': self.optimizer_class.value,
+            'options': self.args,
+        }
+
+
+def _default_arg_names_and_vals(cls):
+    return {
+        p.name: p.default for p in inspect.signature(cls).parameters.values()
+        if p.default != inspect.Parameter.empty
+        and p.kind == p.POSITIONAL_OR_KEYWORD
+    }
 
 
 DEFAULT_ADAM_CONF = OptimizerConfiguration(
     OPTIMIZER.ADAM,
-    dict(
-        {
-            'lr': 1e-3,
-            'betas': (0.9, 0.999),
-            'eps': 1e-8,
-            'weight_decay': 0,
-        }
-    ),
+    'torch.optim',
+    _default_arg_names_and_vals(Adam),
 )
